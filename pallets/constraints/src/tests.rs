8a64885c4bc048a91d395b68313527d7563b0efe
JesseAbram <jesseabramowitz@hotmail.com>
2021-10-29 09:21:46 -0400

    add whitelist constraint

--- pallets/constraints/src/tests.rs
+++ pallets/constraints/src/tests.rs
@@ -1,20 +1,21 @@
 use crate::{mock::*, Error};
 use frame_support::{assert_noop, assert_ok};
 
+// adds whitelist and checks max whitelist and already whitelisted
 #[test]
-fn it_works_for_default_value() {
+fn whitelist_address() {
 	new_test_ext().execute_with(|| {
-		// Dispatch a signed extrinsic.
-		assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
-		// Read pallet storage and assert an expected result.
-		assert_eq!(TemplateModule::something(), Some(42));
-	});
-}
-
-#[test]
-fn correct_error_for_none_value() {
-	new_test_ext().execute_with(|| {
-		// Ensure the expected error is thrown when no value is present.
-		assert_noop!(TemplateModule::cause_error(Origin::signed(1)), Error::<Test>::NoneValue);
+		assert_ok!(Constraints::add_whitelist_address(Origin::signed(1), 2));
+		assert_eq!(Constraints::address_whitelist(1), [2]);
+		assert_noop!(
+			Constraints::add_whitelist_address(Origin::signed(1), 2),
+			Error::<Test>::AlreadyWhitelisted
+		);
+		assert_ok!(Constraints::add_whitelist_address(Origin::signed(1), 3));
+		assert_eq!(Constraints::address_whitelist(1), [2, 3]);
+		assert_noop!(
+			Constraints::add_whitelist_address(Origin::signed(1), 4),
+			Error::<Test>::MaxWhitelist
+		);
 	});
 }
