06712c3579cf33437c04cfe14e9c8984df3109ec
JesseAbram <jesseabramowitz@hotmail.com>
2021-11-09 12:32:10 -0500

    added bash script to pull metadata

---
+++ scripts/pull_entropy_metadata.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -sX POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"state_getMetadata", "id": 1}' localhost:9933 \
+                     | jq .result \
+                     | cut -d '"' -f 2 \
+                     | xxd -r -p > ./subxttest/src/entropy_metadata.scale
