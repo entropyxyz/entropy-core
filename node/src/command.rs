1b07ac3a1197143bf00b52439ac94fd9f1b57662
JesseAbram <jesseabramowitz@hotmail.com>
2021-11-09 12:01:58 -0500

    upgrade chain to master

--- node/src/command.rs
+++ node/src/command.rs
@@ -81,11 +81,7 @@ pub fn run() -> Result<()> {
 		None => {
 			let runner = cli.create_runner(&cli.run)?;
 			runner.run_node_until_exit(|config| async move {
-				match config.role {
-					Role::Light => service::new_light(config),
-					_ => service::new_full(config),
-				}
-				.map_err(sc_cli::Error::Service)
+				service::new_full(config).map_err(sc_cli::Error::Service)
 			})
 		},
 		Some(Subcommand::Benchmark(cmd)) =>
