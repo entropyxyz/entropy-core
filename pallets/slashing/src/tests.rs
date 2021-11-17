b1b366e1d65d58fb8c6e230cda05c1593b80b1b0
JesseAbram <33698952+JesseAbram@users.noreply.github.com>
2021-11-17 11:58:22 -0500

    Slashing tests (#18)
    * propgation mocks fixed
    
    * slashing tests
    
    * fix build
    
    * fix all tests
    
    * test slash fraction
    
    * tests for propogation

--- pallets/slashing/src/tests.rs
+++ pallets/slashing/src/tests.rs
@@ -1,9 +1,12 @@
+use super::*;
 use crate::{mock::*, Error};
 use frame_support::{assert_noop, assert_ok};
+use sp_runtime::Perbill;
+use sp_staking::offence::Offence;
 
 #[test]
-fn it_works_for_default_value() {
+fn slash_fraction_works() {
 	new_test_ext().execute_with(|| {
-
+		assert_eq!(TuxAngry::<()>::slash_fraction(1, 2), Perbill::from_perthousand(500));
 	});
 }
