5956d5d386a95a1333b474bd9b903192c5ccac14
Thor <thorck@protonmail.com>
2022-02-02 18:55:08 -0800

    setup clap CLI

--- crypto/protocol/src/keygen/mod.rs
+++ crypto/protocol/src/keygen/mod.rs
@@ -1,10 +1,6 @@
-//! Alice generates the keys
+//! Alice generates the keys.
+//! Reference heavily:
+//! https://github.com/ZenGo-X/multi-party-ecdsa/blob/master/examples/gg20_keygen.rs
 use super::user::User;
 #[allow(unused_imports)]
 use multi_party_ecdsa::protocols::multi_party_ecdsa::gg_2020::state_machine::keygen::Keygen;
-
-impl User {
-	pub fn keygen() {
-		todo!();
-	}
-}
--- crypto/protocol/src/lib.rs
+++ crypto/protocol/src/lib.rs
@@ -3,5 +3,35 @@ pub mod node;
 pub mod signing;
 pub mod user;
 /// Generate `num_parties` of `threshold` key shares
-pub const NUM_PARTIES: u16 = 6;
+use clap::{AppSettings, Parser, Subcommand};
+
+pub const SHARES: u16 = 6;
 pub const THRESHOLD: u16 = 6;
+
+/// Arguments to CLI, default to 6 of 6.
+#[derive(Parser, Debug)]
+#[clap(name = "entro", version, about = "Entropy cryptography CLI")]
+struct Cli {
+	commands: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+	#[clap(setting(AppSettings::ArgRequiredElseHelp))]
+	Keygen {
+		#[clap(short, long, default_value_t = SHARES)]
+		shares: u8,
+		#[clap(short, long, default_value_t = THRESHOLD)]
+		threshold: u8,
+	},
+	// todo: sign tx, delete acct
+}
+
+fn main() {
+	let args = Args::parse();
+	match &args.command {
+		Commands::Keygen { shares, threshold } => {
+			// Do keygen
+		},
+	}
+}
