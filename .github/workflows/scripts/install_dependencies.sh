f4a4f071be73546625550f1a8b01e720cb9eb7e0
Kara Graysen <kara@noisypigeon.inc>
2022-02-03 13:07:46 -0500

    redo rust install commands

--- .github/workflows/scripts/install_dependencies.sh
+++ .github/workflows/scripts/install_dependencies.sh
@@ -3,6 +3,8 @@
 set -e
 
 sudo apt install -y libssl-dev clang libclang-dev
+rustup default nightly-x86_64-unknown-linux-gnu
+rustup update
 rustup update nightly
 rustup target add wasm32-unknown-unknown --toolchain nightly
 
