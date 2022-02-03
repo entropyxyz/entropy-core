bb61535a677958b0e73ac9b9d907aece81212007
Thor <thorck@protonmail.com>
2022-02-02 19:33:06 -0800

    keygen async setup from Clap

--- crypto/protocol/src/keygen/mod.rs
+++ crypto/protocol/src/keygen/mod.rs
@@ -1,6 +1,40 @@
 //! Alice generates the keys.
 //! Reference heavily:
 //! https://github.com/ZenGo-X/multi-party-ecdsa/blob/master/examples/gg20_keygen.rs
-use super::user::User;
-#[allow(unused_imports)]
-use multi_party_ecdsa::protocols::multi_party_ecdsa::gg_2020::state_machine::keygen::Keygen;
+#![allow(unused_imports)]
+use anyhow::{anyhow, Context, Result};
+// use futures::StreamExt;
+use std::path::PathBuf;
+// use multi_party_ecdsa::protocols::multi_party_ecdsa::gg_2020::state_machine::keygen::Keygen;
+// use round_based::async_runtime::AsyncProtocol;
+
+// mod gg20_sm_client;
+// use gg20_sm_client::join_computation;
+
+pub async fn keygen(shares: &u8, threshold: &u8, output: &PathBuf) -> Result<()> {
+	let mut output_file = tokio::fs::OpenOptions::new()
+		.write(true)
+		.create_new(true)
+		.open(output)
+		.await
+		.context("cannot create output file")?;
+
+	// let (_i, incoming, outgoing) =
+	// 	join_computation(args.address, &args.room).await.context("join computation")?;
+
+	// let incoming = incoming.fuse();
+	// tokio::pin!(incoming);
+	// tokio::pin!(outgoing);
+
+	// let keygen = Keygen::new(args.index, args.threshold, args.number_of_parties)?;
+	// let output = AsyncProtocol::new(keygen, incoming, outgoing)
+	// 	.run()
+	// 	.await
+	// 	.map_err(|e| anyhow!("protocol execution terminated with error: {}", e))?;
+	// let output = serde_json::to_vec_pretty(&output).context("serialize output")?;
+	// tokio::io::copy(&mut output.as_slice(), &mut output_file)
+	// 	.await
+	// 	.context("save output to file")?;
+
+	Ok(())
+}
--- crypto/protocol/src/lib.rs
+++ crypto/protocol/src/lib.rs
@@ -4,34 +4,41 @@ pub mod signing;
 pub mod user;
 /// Generate `num_parties` of `threshold` key shares
 use clap::{AppSettings, Parser, Subcommand};
+use std::path::PathBuf;
 
-pub const SHARES: u16 = 6;
-pub const THRESHOLD: u16 = 6;
+pub const SHARES: u8 = 6;
+pub const THRESHOLD: u8 = 6;
 
 /// Arguments to CLI, default to 6 of 6.
 #[derive(Parser, Debug)]
 #[clap(name = "entro", version, about = "Entropy cryptography CLI")]
 struct Cli {
+	#[clap(subcommand)]
 	commands: Commands,
 }
 
-#[derive(Subcommand)]
+#[derive(Debug, Subcommand)]
 enum Commands {
+	/// Generate `threshold` of `output` keyshares at `output`
 	#[clap(setting(AppSettings::ArgRequiredElseHelp))]
 	Keygen {
 		#[clap(short, long, default_value_t = SHARES)]
 		shares: u8,
 		#[clap(short, long, default_value_t = THRESHOLD)]
 		threshold: u8,
+		/// Where keys are written to
+		#[clap(short, long)]
+		output: PathBuf,
 	},
 	// todo: sign tx, delete acct
 }
 
-fn main() {
-	let args = Args::parse();
-	match &args.command {
-		Commands::Keygen { shares, threshold } => {
-			// Do keygen
+#[tokio::main]
+async fn main() {
+	let args = Cli::parse();
+	match &args.commands {
+		Commands::Keygen { shares, threshold, output } => {
+			keygen::keygen(shares, threshold, output);
 		},
 	}
 }
