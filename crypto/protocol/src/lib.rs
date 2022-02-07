e862bc5eff2a06f7f7923d18b817b075241187a6
Thor <thorck@protonmail.com>
2022-02-07 12:52:18 -0800

    yoink, structopt

--- crypto/protocol/src/lib.rs
+++ crypto/protocol/src/lib.rs
@@ -1,50 +1,43 @@
+#![allow(unused_imports)]
 pub mod keygen;
 pub mod node;
 pub mod signing;
 pub mod user;
-use clap::{AppSettings, Parser, Subcommand};
-use std::path::PathBuf;
+//use std::path::PathBuf;
+use structopt::StructOpt;
+use keygen::KeygenCli;
+use anyhow::{anyhow, Context, Result};
+
 
 /// Generate `SHARES` of `threshold` key shares
-pub const SHARES: u16 = 6;
+pub const N_PARTIES: u16 = 6;
 pub const THRESHOLD: u16= 6;
 
-/// Arguments to CLI, default to 6 of 6. Usage:
-/// `$ entro <subcommand>`
-#[derive(Parser, Debug)]
-#[clap(name = "entro", version, about = "Entropy cryptography CLI")]
-struct Cli {
-	#[clap(subcommand)]
-	commands: Commands,
+#[derive(Debug,StructOpt)]
+struct Cli{
+	#[structopt(subcommand)]
+	cmd: Command 
 }
 
-#[derive(Debug, Subcommand)]
-enum Commands {
+#[derive(StructOpt, Debug)]
+enum Command {
 	/// Generate `threshold` of `output` keyshares at `output`
-	#[clap(setting(AppSettings::ArgRequiredElseHelp))]
-	Keygen {
-		#[clap(short, long, default_value_t = SHARES)]
-		shares: u16,
-		#[clap(short, long, default_value_t = THRESHOLD)]
-		threshold: u16,
-		/// Where keys are written to
-		#[clap(short, long)]
-		output: PathBuf,
-	},
+	Keygen ( KeygenCli),
 	Sign,
 	DeleteAccount
 	// todo: sign tx, delete acct
 }
 
 #[tokio::main]
-async fn main() {
-	let args = Cli::parse();
-	match &args.commands {
+async fn main() -> Result<()> {
+	let args: Cli = Cli::from_args();
+	match &args.cmd {
  
-		Commands::Keygen { shares, threshold, output } => {
-			keygen::keygen(shares, threshold, output);
+		Command::Keygen(keygen_cli) => {
+			keygen::keygen(keygen_cli);
 		},
-		Commands::Sign => todo!(), 
-		Commands::DeleteAccount => todo!(),
+		Command::Sign => todo!(), 
+		Command::DeleteAccount => todo!(),
 	}
+	Ok(())
 }
