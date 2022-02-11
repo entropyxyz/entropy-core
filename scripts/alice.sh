b54bb0214693ace21e59055ed8a8c046b08b210c
JesseAbram <33698952+JesseAbram@users.noreply.github.com>
2022-02-11 13:02:08 -0500

    alice and bob (#83)

---
+++ scripts/alice.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+./target/release/node-template purge-chain --base-path /tmp/alice --chain local -y
+
+./target/release/entropy \
+--base-path /tmp/alice \
+--chain local \
+--alice \
+--port 30333 \
+--ws-port 9944 \
+--rpc-port 9933 \
+--node-key 0000000000000000000000000000000000000000000000000000000000000001 \
+--validator
+
