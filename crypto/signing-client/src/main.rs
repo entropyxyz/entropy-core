2eab8bf63f03c51e1568e9f1b5ec00b454cee2d0
David <818daf@gmail.com>
2022-02-15 00:26:21 +0100

    add gg20-sm-manager's routines to signing-client

--- crypto/signing-client/src/main.rs
+++ crypto/signing-client/src/main.rs
@@ -8,9 +8,15 @@ mod com_manager;
 mod sign;
 mod store_share;
 
+
+use com_manager::{subscribe, issue_idx, broadcast};
 // ToDo: JA add proper response types and formalize them across all endpoints
 
 #[launch]
 fn rocket() -> _ {
-	rocket::build().mount("/", routes![store_keyshare, provide_share, start_com_manager,])
+	rocket::build().mount("/", routes![
+		store_keyshare, 
+		provide_share, 
+		start_com_manager,
+		subscribe, issue_idx, broadcast])
 }
