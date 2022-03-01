6329db350663d717ae1ec8ad2561b48dcef76d21
JesseAbram <jesseabramowitz@hotmail.com>
2022-03-01 11:38:06 -0500

    fix non compilation

--- crypto/testing-user-clients/src/testregister.rs
+++ crypto/testing-user-clients/src/testregister.rs
@@ -4,9 +4,8 @@ use protocol::user::User;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	println!("test_register");
 	let user = User {};
-	let reg_res = user.send_registration_request().await?;
-
-	println!("reg_res.signing_nodes {}", reg_res.signing_nodes);
+	let _reg_res = user.send_registration_request().await?;
+	println!("test_register");
 	//ToDo:
 	// send key to bob
 	alice_send::send().await;
