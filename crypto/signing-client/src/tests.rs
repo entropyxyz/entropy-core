a0b56350dbb3ccf6a6ec6d80f458fa59fde4a094
JesseAbram <jesseabramowitz@hotmail.com>
2022-03-02 16:55:03 -0500

    signing client tests

---
+++ crypto/signing-client/src/tests.rs
@@ -0,0 +1,63 @@
+use crate as main;
+use super::rocket;
+use rocket::local::asynchronous::Client;
+use rocket::http::{Status, ContentType};
+use curv::elliptic::curves::secp256_k1::Secp256k1;
+use multi_party_ecdsa::protocols::multi_party_ecdsa::gg_2020::state_machine::keygen::LocalKey;
+use std::{env, fs::remove_file};
+
+async fn setup_client() -> rocket::local::asynchronous::Client {
+	Client::tracked(super::rocket()).await
+		.expect("valid `Rocket`")
+}
+
+fn get_path(extension: &str) -> String {
+	let path = env::current_dir();
+
+	let mut file_path: String = path.unwrap().as_path().display().to_string().to_owned();
+	file_path.push_str(extension);
+	file_path
+}
+
+#[rocket::async_test]
+async fn test_store_share() {
+
+
+	let file_path = get_path("/src/mocks/local-share1.json");
+
+	let file = tokio::fs::read(file_path).await;
+
+	let json: LocalKey<Secp256k1> = serde_json::from_slice(&file.unwrap()).unwrap();
+	 // Construct a client to use for dispatching requests.
+	 let client = setup_client().await;
+	 let response = client.post("/store_keyshare")
+	 .header(ContentType::JSON)
+	 .body(serde_json::to_string(&json).unwrap())
+	 .dispatch().await;
+
+	 assert_eq!(response.status(), Status::Ok);
+
+	let new_path = get_path("/local-share2.json");
+
+	// check to see if new file was stored
+    let result = remove_file(new_path);
+	assert_eq!(result.is_ok(), true);
+
+}
+
+
+#[rocket::async_test]
+async fn test_store_share_fail() {
+	 // Construct a client to use for dispatching requests.
+	 let client = setup_client().await;
+
+	 let response = client.post("/store_keyshare")
+	 .header(ContentType::JSON)
+	 .body(r##"{
+		"name": "John Doe",
+		"email": "j.doe@m.com",
+		"password": "123456"
+	}"##)
+	 .dispatch().await;
+	 assert_eq!(response.status(), Status::UnprocessableEntity);
+}
