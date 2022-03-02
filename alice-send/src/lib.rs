35bd15e00955ddd35a451cb08f7894fbf2afda38
David <818daf@gmail.com>
2022-03-02 18:05:09 +0100

    add test for alice-send

--- alice-send/src/lib.rs
+++ alice-send/src/lib.rs
@@ -4,14 +4,15 @@ use reqwest;
 
 // mod main;
 
-pub async fn send() -> Result<(), Box<dyn std::error::Error>> {
+pub async fn send(url: String, path: String) -> Result<reqwest::Response, Box<dyn std::error::Error>> {
 	println!("sending keyshare...");
 
 	// let file1 = tokio::fs::read("./alice-send/local-share1.json").await?;
-	let file2 = tokio::fs::read("./alice-send/local-share2.json").await?;
+	let file2 = tokio::fs::read(path).await?;
+
 	// let json1: LocalKey<Secp256k1> = serde_json::from_slice(&file1).unwrap();
 	let json2: LocalKey<Secp256k1> = serde_json::from_slice(&file2).unwrap();
-
+	
 	let client = reqwest::Client::new();
 	// // send to Bob
 	// let res = client
@@ -24,46 +25,36 @@ pub async fn send() -> Result<(), Box<dyn std::error::Error>> {
 
 	// send to Charlie
 	let res2 = client
-		.post("http://127.0.0.1:3002/store_keyshare")
+		.post(url)
 		.header("Content-Type", "application/json")
 		.json(&json2)
 		.send()
 		.await?;
-	println!("{:?}", res2);
-	Ok(())
+	
+	Ok(res2)
 }
 
 #[cfg(test)]
 mod tests {
 	use super::*;
 	use httpmock::prelude::*;
-	use isahc::config::RedirectPolicy;
-	use isahc::prelude::*;
-	use isahc::HttpClientBuilder;
 
-	#[test]
-	fn test_dummy(){
-		assert_eq!(1+1,2);
-	}
-
-	#[test]
-	fn test_user_sending_keyshare(){
-		assert_eq!(1+1,2);
-
-		// create new server and define this server's methods
+	#[tokio::test]
+	async fn test_user_sending_keyshare() -> Result<(), Box<dyn std::error::Error>>{
+		// create new server and define this server's methods and responses
 		let server1 = MockServer::start();
 		let receiving_mock = server1.mock(|when, then| {
-			when.path("/store_keyshare");
+			when.path("/store_keyshare")
+				.method(POST)
+				.header("Content-Type", "application/json")
+				;
 			then.status(200)
 				.body("stuff");
 		});
 
-		let http_client = HttpClientBuilder::new()
-			.redirect_policy(RedirectPolicy::Follow)
-			.build()
-			.unwrap();
+		let response = send(server1.url("/store_keyshare"), String::from("./local-share2.json")).await?;
+		println!("response: {:?}",&response);
 
-		let response = http_client.get(server1.url("/store_keyshare")).unwrap();
 		// the response should look like this: 
 		// Response { url: Url { scheme: "http", cannot_be_a_base: false, username: "", password: None, host: Some(Ipv4(127.0.0.1)), port: Some(3002), path: "/store_keyshare", query: None, fragment: None }, status: 200, headers: {"server": "Rocket", "x-content-type-options": "nosniff", "x-frame-options": "SAMEORIGIN", "permissions-policy": "interest-cohort=()", "content-length": "0", "date": "Wed, 02 Mar 2022 15:15:18 GMT"} }
 
@@ -73,7 +64,7 @@ mod tests {
 		// assert that the response is correct
 		// ToDo: additional checks
 		assert_eq!(response.status(),200);
-	
+		Ok(())
 
 	}
 }
