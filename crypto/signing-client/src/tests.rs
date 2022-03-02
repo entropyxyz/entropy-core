43c66372c9dacdb3b68b2df7e5ae713196c90de9
JesseAbram <jesseabramowitz@hotmail.com>
2022-03-02 16:56:13 -0500

    fmt

--- crypto/signing-client/src/tests.rs
+++ crypto/signing-client/src/tests.rs
@@ -1,14 +1,15 @@
-use crate as main;
 use super::rocket;
-use rocket::local::asynchronous::Client;
-use rocket::http::{Status, ContentType};
+use crate as main;
 use curv::elliptic::curves::secp256_k1::Secp256k1;
 use multi_party_ecdsa::protocols::multi_party_ecdsa::gg_2020::state_machine::keygen::LocalKey;
+use rocket::{
+	http::{ContentType, Status},
+	local::asynchronous::Client,
+};
 use std::{env, fs::remove_file};
 
 async fn setup_client() -> rocket::local::asynchronous::Client {
-	Client::tracked(super::rocket()).await
-		.expect("valid `Rocket`")
+	Client::tracked(super::rocket()).await.expect("valid `Rocket`")
 }
 
 fn get_path(extension: &str) -> String {
@@ -21,43 +22,45 @@ fn get_path(extension: &str) -> String {
 
 #[rocket::async_test]
 async fn test_store_share() {
-
-
 	let file_path = get_path("/src/mocks/local-share1.json");
 
 	let file = tokio::fs::read(file_path).await;
 
 	let json: LocalKey<Secp256k1> = serde_json::from_slice(&file.unwrap()).unwrap();
-	 // Construct a client to use for dispatching requests.
-	 let client = setup_client().await;
-	 let response = client.post("/store_keyshare")
-	 .header(ContentType::JSON)
-	 .body(serde_json::to_string(&json).unwrap())
-	 .dispatch().await;
+	// Construct a client to use for dispatching requests.
+	let client = setup_client().await;
+	let response = client
+		.post("/store_keyshare")
+		.header(ContentType::JSON)
+		.body(serde_json::to_string(&json).unwrap())
+		.dispatch()
+		.await;
 
-	 assert_eq!(response.status(), Status::Ok);
+	assert_eq!(response.status(), Status::Ok);
 
 	let new_path = get_path("/local-share2.json");
 
 	// check to see if new file was stored
-    let result = remove_file(new_path);
+	let result = remove_file(new_path);
 	assert_eq!(result.is_ok(), true);
-
 }
-
 
 #[rocket::async_test]
 async fn test_store_share_fail() {
-	 // Construct a client to use for dispatching requests.
-	 let client = setup_client().await;
+	// Construct a client to use for dispatching requests.
+	let client = setup_client().await;
 
-	 let response = client.post("/store_keyshare")
-	 .header(ContentType::JSON)
-	 .body(r##"{
+	let response = client
+		.post("/store_keyshare")
+		.header(ContentType::JSON)
+		.body(
+			r##"{
 		"name": "John Doe",
 		"email": "j.doe@m.com",
 		"password": "123456"
-	}"##)
-	 .dispatch().await;
-	 assert_eq!(response.status(), Status::UnprocessableEntity);
+	}"##,
+		)
+		.dispatch()
+		.await;
+	assert_eq!(response.status(), Status::UnprocessableEntity);
 }
