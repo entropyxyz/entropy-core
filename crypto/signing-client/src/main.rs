99c0715c4cc0cfb2374e2cdc94722d93bbaed2e3
JesseAbram <33698952+JesseAbram@users.noreply.github.com>
2022-05-02 09:09:30 -0400

    Mnemonic passing (#120)
    * add in confirm done function
    
    * add a threshold signing key to staking
    
    * add threshold key check to confirm done
    
    * fix mock for relayer
    
    * test relayer changes
    
    * add threshold account to build
    
    * add custom mneumonic to singing
    
    * fix config
    
    * fix test
    
    * fmt
    
    * add todos
    
    * fix test mocks
    
    * add global mneuomic
    
    * add env mneumonic
    
    * add a mnemonic check on spin up
    
    * add endpoint to config
    
    * fmt

--- crypto/signing-client/src/main.rs
+++ crypto/signing-client/src/main.rs
@@ -1,8 +1,13 @@
 use crate::{sign::provide_share, store_share::store_keyshare};
+use bip39::{Language, Mnemonic};
 use rocket::routes;
+use serde::Deserialize;
+use std::env;
 
 #[macro_use]
 extern crate rocket;
+
+use rocket::State;
 
 #[cfg(test)]
 mod tests;
@@ -17,8 +22,29 @@ mod store_share;
 use com_manager::{broadcast, issue_idx, subscribe, Db};
 // ToDo: JA add proper response types and formalize them across all endpoints
 
+#[derive(Debug, Clone)]
+pub struct Global {
+	mnemonic: String,
+	endpoint: String,
+}
+
+fn default_endpoint() -> Option<String> {
+	Some("ws://localhost:9944".to_string())
+}
+
+#[derive(Deserialize, Debug, Clone)]
+struct Configuration {
+	#[serde(default = "default_endpoint")]
+	endpoint: Option<String>,
+	mnemonic: String,
+}
+
 #[launch]
 fn rocket() -> _ {
+	let c = load_environment_variables();
+
+	let global =
+		Global { mnemonic: c.mnemonic.to_string(), endpoint: c.endpoint.unwrap().to_string() };
 	rocket::build()
 		.mount(
 			"/",
@@ -32,4 +58,22 @@ fn rocket() -> _ {
 			],
 		)
 		.manage(Db::empty())
+		.manage(global)
+}
+
+fn load_environment_variables() -> Configuration {
+	let c;
+
+	if cfg!(test) {
+		c = Configuration {
+			mnemonic:
+				"alarm mutual concert decrease hurry invest culture survey diagram crash snap click"
+					.to_string(),
+			endpoint: Some("ws://localhost:9944".to_string()),
+		}
+	} else {
+		c = envy::from_env::<Configuration>().expect("Please provide MNEMONIC as env var");
+	}
+	assert!(Mnemonic::validate(&c.mnemonic, Language::English).is_ok(), "MNEMONIC is incorrect");
+	c
 }
