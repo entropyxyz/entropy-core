f29ee45133cd48fc5fcd8b830e10ea8d86baca24
JesseAbram <33698952+JesseAbram@users.noreply.github.com>
2022-05-17 11:41:30 -0400

    Request guards (#123)
    * request guard and error skeleton
    
    * guard test
    
    * fmt

---
+++ crypto/signing-client/src/request_guards.rs
@@ -0,0 +1,32 @@
+use crate::errors::SignedMessageError;
+use rocket::http::Status;
+use rocket::request::{self, FromRequest, Outcome, Request};
+
+pub struct SignedMessage<'r>(&'r str);
+
+pub fn is_valid(signature: &str) -> bool {
+	true
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for SignedMessage<'r> {
+	type Error = SignedMessageError;
+
+	async fn from_request(req: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
+		match req.headers().get_one("signed-message") {
+			None => Outcome::Failure((Status::BadRequest, SignedMessageError::Missing)),
+			Some(message) if is_valid(message) => Outcome::Success(SignedMessage(message)),
+			Some(_) => Outcome::Failure((Status::BadRequest, SignedMessageError::Invalid)),
+		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn is_valid_test() {
+		assert_eq!(is_valid("test"), true);
+	}
+}
