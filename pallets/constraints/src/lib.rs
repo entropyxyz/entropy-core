2f84733075487adbf9830d52d7c4da284af90227
JesseAbram <jesseabramowitz@hotmail.com>
2022-06-24 16:30:29 -0400

    constraints

--- pallets/constraints/src/lib.rs
+++ pallets/constraints/src/lib.rs
@@ -1,8 +1,18 @@
-#![cfg_attr(not(feature = "std"), no_std)]
+//! # Constraint Pallet
+//!
+//! - [`Pallet`]
+//!
+//! ## Overview
+//!
+//! Holds the onchain constraints for users
+//!
+//!
+//! ### Public Functions
+//!
+//! add_whitelist_address - lets a user add a whitelisted address to their account
+//!
 
-/// Edit this file to define custom logic or remove it if it is not needed.
-/// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// <https://substrate.dev/docs/en/knowledgebase/runtime/frame>
+#![cfg_attr(not(feature = "std"), no_std)]
 pub use pallet::*;
 
 #[cfg(test)]
@@ -19,12 +29,10 @@ pub mod pallet {
 	use frame_support::{dispatch::DispatchResult, inherent::Vec, pallet_prelude::*};
 	use frame_system::pallet_prelude::*;
 	use sp_runtime::sp_std::str;
-	/// Configure the pallet by specifying the parameters and types on which it depends.
+
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
-		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-
 		type MaxWhitelist: Get<u32>;
 		type MaxAddressLength: Get<u32>;
 	}
@@ -34,17 +42,12 @@ pub mod pallet {
 	#[pallet::without_storage_info]
 	pub struct Pallet<T>(_);
 
-	// #[pallet::hooks]
-	// impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
-
 	#[pallet::storage]
 	#[pallet::getter(fn address_whitelist)]
 	/// Mapping of whitelisted addresses
 	pub type AddressWhitelist<T: Config> =
 		StorageMap<_, Blake2_128Concat, T::AccountId, Vec<Vec<u8>>, ValueQuery>;
 
-	// Pallets use events to inform users when important changes are made.
-	// https://substrate.dev/docs/en/knowledgebase/runtime/events
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
@@ -52,23 +55,20 @@ pub mod pallet {
 		AddressesWhitelisted(T::AccountId, Vec<Vec<u8>>),
 	}
 
-	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
-		/// Error names should be descriptive.
-		NoneValue,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
+		/// Max amount of whitelisted addresses reached.
 		MaxWhitelist,
+		/// Address already whitelisted.
 		AlreadyWhitelisted,
+		/// Address to whitelist is too long.
 		AddressTooLong,
 	}
 
-	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-	// These functions materialize as "extrinsics", which are often compared to transactions.
-	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
+		/// Adds an address to be whitelisted
+		/// - `whitelist_addresses`: Addresses to be whitelisted
 		#[pallet::weight((10_000 + T::DbWeight::get().writes(1), Pays::No))]
 		pub fn add_whitelist_address(
 			origin: OriginFor<T>,
