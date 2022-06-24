f9f38eae5443bb01d869837ac3f3d50d897e1600
thor <thorck@protonmail.com>
2022-06-23 17:09:42 -0700

    remove testing user clients

--- crypto/testing-user-clients/src/testsign.rs
+++
@@ -1,9 +0,0 @@
-// use protocol::user::User;
-
-// #[async_std::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-// 	println!("test_sign");
-// 	let user = User {};
-// 	user.sign_message(1).await?;
-// 	Ok(())
-// }
