a072d23684e192174e2d6cc414c1a725e98415b7
Rhu <103735902+heyitsrhu@users.noreply.github.com>
2022-07-21 16:04:14 -0400

    Add CircleCI configuration (#142)
    * Add starter CircleCI configuration (#141)
    
    * Clean up and DRY up CircleCI configuration (#143)
    
    * Clean up and DRY up config
    
    * Fix env vars (doesn't work)
    
    * trigger build
    
    Co-authored-by: Kara Graysen <kara@noisypigeon.me>
    Co-authored-by: Kara Graysen <kara@noisypigeon.inc>

--- .github/workflows/scripts/install_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-sudo apt install -y libssl-dev clang libclang-dev
-rustup default stable
-rustup update nightly
-rustup update stable
-rustup target add wasm32-unknown-unknown --toolchain nightly
