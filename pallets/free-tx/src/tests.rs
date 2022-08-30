5b1662f0309736a7fba13a9a758400102845fb7a
Jake Hemmerle <jakehemmerle@protonmail.com>
2022-08-30 14:06:55 -0400

    updated tests, need to fix era updates in tests

--- pallets/free-tx/src/tests.rs
+++ pallets/free-tx/src/tests.rs
@@ -1,16 +1,17 @@
 use frame_support::{assert_err, assert_ok};
 use mock::{
-  new_test_ext, start_active_era, Call, Event as TestEvent, FreeTx, Origin, System, SystemCall,
-  Test,
+  new_test_ext, run_to_block, start_active_era, Call, Event as TestEvent, FrameStaking, FreeTx,
+  Origin, System, SystemCall, Test,
 };
 use sp_runtime::{DispatchError, ModuleError};
 
 use super::*;
+use crate::StakingCurrentEra;
 
 #[test]
 fn try_free_call_works() {
   new_test_ext().execute_with(|| {
-    // first block doesn't emit events, and we need an era index
+    // must be in an era for free calls to be enabled
     start_active_era(1);
     // enable free calls (1 free call per era)
     FreeCallsPerEra::<Test>::put(1 as FreeCallCount);
@@ -27,7 +28,7 @@ fn try_free_call_works() {
 #[test]
 fn try_free_call_errors_when_child_call_errors() {
   new_test_ext().execute_with(|| {
-    // first block doesn't emit events, and we need an era index
+    // must be in an era for free calls to be enabled
     start_active_era(1);
     // enable free calls (1 free call per era)
     FreeCallsPerEra::<Test>::put(1 as FreeCallCount);
@@ -49,7 +50,7 @@ fn try_free_call_errors_when_child_call_errors() {
 #[test]
 fn try_free_call_errors_when_no_free_calls_left() {
   new_test_ext().execute_with(|| {
-    // first block doesn't emit events, and we need an era index
+    // must be in an era for free calls to be enabled
     start_active_era(1);
     // enable free calls (1 free call per era)
     FreeCallsPerEra::<Test>::put(1 as FreeCallCount);
@@ -78,8 +79,7 @@ fn try_free_call_errors_when_no_free_calls_left() {
 #[test]
 fn try_free_call_still_consumes_a_free_call_on_child_fail() {
   new_test_ext().execute_with(|| {
-    // Set block number to 1 because events are not emitted on block 0.
-    // first block doesn't emit events, and we need an era index
+    // must be in an era for free calls to be enabled
     start_active_era(1);
     // enable free calls (1 free call per era)
     FreeCallsPerEra::<Test>::put(1 as FreeCallCount);
@@ -103,59 +103,43 @@ fn try_free_call_still_consumes_a_free_call_on_child_fail() {
 }
 
 #[test]
-fn set_free_calls_per_era_updates_every_era() {
+fn free_calls_refresh_every_era() {
   new_test_ext().execute_with(|| {
-    // Set block number to 1 because events are not emitted on block 0 and we need an era index
-    start_active_era(1);
-
-    let _ = FreeTx::set_free_calls_per_era(Origin::root(), 1);
-    assert_eq!(FreeTx::free_calls_remaining(&1u64), 1 as FreeCallCount);
-
-    let _ = FreeTx::set_free_calls_per_era(Origin::root(), 5);
-    assert_eq!(FreeTx::free_calls_remaining(&1u64), 5 as FreeCallCount);
-
-    let _ = FreeTx::set_free_calls_per_era(Origin::root(), 0);
-    assert_eq!(FreeTx::free_calls_remaining(&1u64), 0 as FreeCallCount);
-  });
-}
-
-#[test]
-fn free_calls_can_be_disabled() {
-  new_test_ext().execute_with(|| {
-    // Set block number to 1 because events are not emitted on block 0 and we need an era index
-    start_active_era(1);
+    // must be in an era for free calls to be enabled
+    run_to_block(1);
+    for i in 2..50 {
+      start_active_era(i);
+      println!("{:?}", FrameStaking::current_era());
+      println!("{:?}", <Test as StakingCurrentEra>::current_era());
+    }
+    // start_active_era(10);
+    // start_active_era(15);
 
     // enable free calls
     let _ = FreeTx::set_free_calls_per_era(Origin::root(), 5);
     assert_eq!(FreeTx::free_calls_remaining(&1u64), 5 as FreeCallCount);
 
-    // make a call that works
+    // make a call that works, check call is used
     let call = Box::new(Call::System(SystemCall::remark { remark: b"entropy rocks".to_vec() }));
     assert_ok!(FreeTx::try_free_call(Origin::signed(1), call));
     assert_eq!(FreeTx::free_calls_remaining(&1u64), 4 as FreeCallCount);
 
-    // then, disable free calls and go to new era
-    let _ = FreeTx::set_free_calls_per_era(Origin::root(), 0);
-    start_active_era(2);
+    // start a new era
+    // start_active_era(4);
 
-    // make sure it fails bc free calls are disabled
-    let call = Box::new(Call::System(SystemCall::remark { remark: b"entropy rocks2".to_vec() }));
-    let expected_error = DispatchError::Module(ModuleError {
-      index:   8,
-      error:   [0, 0, 0, 0],
-      message: Some("FreeCallsDisabled"),
-    });
-    assert_err!(FreeTx::try_free_call(Origin::signed(1), call), expected_error);
+    // make sure call count is refreshed
+    println!("{:?}", <Test as StakingCurrentEra>::current_era());
+    assert_eq!(FreeTx::free_calls_remaining(&1u64), 5 as FreeCallCount);
   });
 }
 
 #[test]
 fn free_calls_disabled_by_default() {
   new_test_ext().execute_with(|| {
-    // Set block number to 1 because events are not emitted on block 0 and we need an era index
+    // must be in an era for free calls to be enabled
     start_active_era(1);
 
-    // enable free calls
+    // make sure we have no free calls
     assert_eq!(FreeTx::free_calls_remaining(&1u64), 0 as FreeCallCount);
 
     // make sure it fails bc free calls are disabled
