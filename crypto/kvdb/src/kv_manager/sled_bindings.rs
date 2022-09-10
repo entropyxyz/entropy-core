058f81aa1796f7ce47aca14b421c801874874e0e
John Sahhar <john@sahhar.io>
2022-09-10 09:03:37 -0700

    kv_manager/{kv, sled_bindings}.rs add error handling, logging & refactor
    Adds error handling and logging for when `handle_*` fails
    Refactors repeated code
    
    Signed-off-by: John Sahhar <john@sahhar.io>

--- crypto/kvdb/src/kv_manager/sled_bindings.rs
+++ crypto/kvdb/src/kv_manager/sled_bindings.rs
@@ -1,8 +1,9 @@
 //! Bindings for [sled::Db] operations. Errors are mapped to [super::error::InnerKvError].
 
+use std::fmt::Debug;
+
 use serde::{de::DeserializeOwned, Serialize};
 use tofn::sdk::api::{deserialize, serialize};
-
 use super::{
     error::{InnerKvError::*, InnerKvResult},
     types::{KeyReservation, DEFAULT_RESERVE},
@@ -78,7 +79,7 @@ where
 /// Get the value of an existing key.
 /// Returns [SledErr] of [LogicalErr] on failure.
 pub(super) fn handle_get<V>(kv: &encrypted_sled::Db, key: String) -> InnerKvResult<V>
-where V: DeserializeOwned {
+where V: DeserializeOwned + Debug {
     // try to get value of 'key'
     let value = match kv.get(&key)? {
         Some(bytes) => deserialize(&bytes).ok_or(DeserializationErr)?,
