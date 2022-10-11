25c7927918cb52723eecdc04c0b6116c58c1a927
John Sahhar <john@entropy.xyz>
2022-10-11 01:51:26 +0000

    unsafe api endpoints
    Signed-off-by: John Sahhar <john@entropy.xyz>

--- crypto/server/src/user/unsafe_api.rs
+++ crypto/server/src/user/unsafe_api.rs
@@ -7,7 +7,13 @@ use kvdb::kv_manager::{
     KvManager,
 };
 use log::info;
-use rocket::{http::Status, response::stream::EventStream, serde::json::Json, Shutdown, State};
+use rocket::{
+    http::Status,
+    response::stream::EventStream,
+    serde::json::{to_string, Json},
+    Shutdown, State,
+};
+use serde::{Deserialize, Serialize};
 use sp_core::{sr25519, Pair};
 use subxt::{sp_runtime::AccountId32, DefaultConfig, PairSigner};
 use tracing::instrument;
@@ -20,8 +26,70 @@ use crate::{
     Configuration,
 };
 
-#[get("/dh")]
-pub async fn get_dh(state: &State<KvManager>, config: &State<Configuration>) -> String {
-    let dh_public = state.kv().get("DH_PUBLIC").await.unwrap();
-    hex::encode(&dh_public)
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct UnsafeQuery {
+    pub key: String,
+    pub value: String,
+}
+
+impl UnsafeQuery {
+    pub fn new(key: String, value: String) -> Self { UnsafeQuery { key, value } }
+
+    pub fn to_json(&self) -> String { to_string(self).unwrap() }
+}
+
+#[post("/get", format = "json", data = "<key>")]
+pub async fn get(
+    key: Json<UnsafeQuery>,
+    state: &State<KvManager>,
+    config: &State<Configuration>,
+) -> Vec<u8> {
+    state.kv().get(&key.key.to_owned()).await.unwrap()
+}
+
+#[post("/put", format = "json", data = "<key>")]
+pub async fn put(
+    key: Json<UnsafeQuery>,
+    state: &State<KvManager>,
+    config: &State<Configuration>,
+) -> Status {
+    match state.kv().exists(&key.key.to_owned()).await {
+        Err(v) => {
+            warn!("{}", v);
+            Status::InternalServerError
+        },
+        Ok(v) => {
+            if v {
+                state.kv().delete(&key.key.to_owned()).await.unwrap();
+            }
+            match state.kv().reserve_key(key.key.clone()).await {
+                Ok(v) => {
+                    state.kv().put(v, key.value.as_bytes().to_vec()).await.unwrap();
+                    Status::Ok
+                },
+                Err(v) => {
+                    warn!("{}", v);
+                    Status::InternalServerError
+                },
+            }
+        },
+    }
+}
+
+#[post("/delete", format = "json", data = "<key>")]
+pub async fn delete(
+    key: Json<UnsafeQuery>,
+    state: &State<KvManager>,
+    config: &State<Configuration>,
+) -> Status {
+    state.kv().delete(&key.key.to_owned()).await.unwrap();
+    Status::Ok
+}
+
+#[get("/remove_keys")]
+pub async fn remove_keys(state: &State<KvManager>, config: &State<Configuration>) -> Status {
+    state.kv().delete("DH_PUBLIC").await.unwrap();
+    state.kv().delete("MNEMONIC").await.unwrap();
+    state.kv().delete("SHARED_SECRET").await.unwrap();
+    Status::Ok
 }
