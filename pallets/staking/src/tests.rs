747153a41c5f6ef91be7dcf77a380183d8a2c8fc
JesseAbram <33698952+JesseAbram@users.noreply.github.com>
2022-11-08 20:09:12 -0500

    subgropu fix (#226)
    * subgropu fix
    
    * fmt
    
    * fmt

--- pallets/staking/src/tests.rs
+++ pallets/staking/src/tests.rs
@@ -11,6 +11,8 @@ fn basic_setup_works() {
         assert_eq!(Staking::endpoint_register(6).unwrap(), vec![40]);
         assert_eq!(Staking::threshold_account(5).unwrap().0, 7);
         assert_eq!(Staking::threshold_account(6).unwrap().0, 8);
+        assert_eq!(Staking::threshold_to_stash(7).unwrap(), 5);
+        assert_eq!(Staking::threshold_to_stash(8).unwrap(), 6);
         assert_eq!(Staking::signing_groups(0).unwrap(), vec![1]);
         assert_eq!(Staking::signing_groups(1).unwrap(), vec![2]);
     });
@@ -34,6 +36,7 @@ fn it_takes_in_an_endpoint() {
         ));
         assert_eq!(Staking::endpoint_register(1).unwrap(), vec![20]);
         assert_eq!(Staking::threshold_account(2).unwrap().0, 3);
+        assert_eq!(Staking::threshold_to_stash(3).unwrap(), 2);
         assert_noop!(
             Staking::validate(
                 RuntimeOrigin::signed(4),
@@ -103,6 +106,7 @@ fn it_changes_threshold_account() {
 
         assert_ok!(Staking::change_threshold_accounts(RuntimeOrigin::signed(1), 4, NULL_ARR));
         assert_eq!(Staking::threshold_account(2).unwrap().0, 4);
+        assert_eq!(Staking::threshold_to_stash(4).unwrap(), 2);
 
         assert_noop!(
             Staking::change_threshold_accounts(RuntimeOrigin::signed(4), 5, NULL_ARR),
@@ -131,6 +135,7 @@ fn it_deletes_when_no_bond_left() {
 
         assert_eq!(Staking::endpoint_register(1).unwrap(), vec![20]);
         assert_eq!(Staking::threshold_account(2).unwrap().0, 3);
+        assert_eq!(Staking::threshold_to_stash(3).unwrap(), 2);
 
         let mut lock = Balances::locks(2);
         assert_eq!(lock[0].amount, 100);
@@ -151,6 +156,7 @@ fn it_deletes_when_no_bond_left() {
 
         assert_eq!(Staking::endpoint_register(1).unwrap(), vec![20]);
         assert_eq!(Staking::threshold_account(2).unwrap().0, 3);
+        assert_eq!(Staking::threshold_to_stash(3).unwrap(), 2);
 
         assert_ok!(FrameStaking::unbond(RuntimeOrigin::signed(1), 50u64,));
 
@@ -159,5 +165,6 @@ fn it_deletes_when_no_bond_left() {
         assert_eq!(lock.len(), 0);
         assert_eq!(Staking::endpoint_register(1), None);
         assert_eq!(Staking::threshold_account(2), None);
+        assert_eq!(Staking::threshold_to_stash(3), None);
     });
 }
