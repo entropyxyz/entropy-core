85c6f119ee11eb6b4ee4ed9c6b239a4b6042f5d3
jakehemmerle <jakehemmerle@protonmail.com>
2022-11-16 02:51:59 -0500

    refactoring

--- crypto/constraints/src/tx/utils.rs
+++ crypto/constraints/src/tx/utils.rs
@@ -5,16 +5,16 @@ use crate::tx::{Architecture, BasicTransaction, HasReceiver, HasSender};
 /// Errors related to parsing raw transactions
 #[derive(Debug, PartialEq)]
 pub enum ParsingError {
-    UnintelligableTransactionRequest,
+    UnparsableTransactionRequest,
     UndetectableArchitecture,
 }
 
 /// Takes raw transaction data and parses it into a basic transaction
-pub fn parse_raw_tx_json<A: Architecture>(
+pub fn parse_tx_request_json<A: Architecture>(
     raw_tx: String,
 ) -> Result<BasicTransaction<A>, ParsingError> {
-    let untyped_json_tx = serde_json::from_str(&raw_tx)
-        .map_err(|_e| ParsingError::UnintelligableTransactionRequest)?;
+    let untyped_json_tx =
+        serde_json::from_str(&raw_tx).map_err(|_e| ParsingError::UnparsableTransactionRequest)?;
 
     match from_value::<A::TransactionRequest>(untyped_json_tx) {
         Ok(tx) => Ok(BasicTransaction { from: tx.sender(), to: tx.receiver() }),
