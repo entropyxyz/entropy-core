665ed961ec4fe8a0faaad78c4ccf7b88da1d2df5
Jake Hemmerle <jakehemmerle@protonmail.com>
2022-11-17 15:10:47 -0500

    Parse JSON EVM Transaction Requests (#239)
    * parsing basic evm tx
    
    * cleanup and additional test
    
    * fmt
    
    * clippy
    
    * removed default feats on web3 to make it lighter
    
    * refactoring
    
    * clean up comments
    
    * fmt
    
    * updated test lol
    
    * updated errors
    
    * fix linting
    
    * add TODO about EVM transaction requests

--- crypto/constraints/src/tests.rs
+++ crypto/constraints/src/tests.rs
@@ -1,4 +1,7 @@
-use crate::whitelist::is_on_whitelist;
+use crate::{
+    tx::{evm::EVM, parse_tx_request_json},
+    whitelist::is_on_whitelist,
+};
 
 #[test]
 fn test_whitelist() {
@@ -11,4 +14,62 @@ fn test_whitelist() {
     assert!(is_on_whitelist(list.clone(), &address));
     // not on list
     assert!(!is_on_whitelist(list, &vec![3u8]));
+}
+
+#[test]
+fn should_parse_json_evm_tx_request() {
+    // copied from https://ethereum.org/en/developers/docs/transactions/#whats-a-transaction
+    let evm_tx_request_json = r#"{
+        "from": "0x1923f626bb8dc025849e00f99c25fe2b2f7fb0db",
+        "gas": "0x55555",
+        "maxFeePerGas": "0x1234",
+        "maxPriorityFeePerGas": "0x1234",
+        "input": "0xabcd",
+        "nonce": "0x0",
+        "to": "0x07a565b7ed7d7a678680a4c162885bedbb695fe0",
+        "value": "0x1234"
+    }"#;
+
+    let basic_tx = parse_tx_request_json::<EVM>(evm_tx_request_json.to_string()).unwrap();
+
+    println!("Parsed tx: {:?}", basic_tx);
+}
+
+#[test]
+fn should_error_on_invalid_json_evm_tx_request() {
+    // missing "from" field
+    let evm_tx_request_json = r#"{
+        "gas": "0x55555",
+        "maxFeePerGas": "0x1234",
+        "maxPriorityFeePerGas": "0x1234",
+        "input": "0xabcd",
+        "nonce": "0x0",
+        "to": "0x07a565b7ed7d7a678680a4c162885bedbb695fe0",
+        "value": "0x1234"
+    }"#;
+
+    let basic_tx_result = parse_tx_request_json::<EVM>(evm_tx_request_json.to_string());
+
+    assert!(basic_tx_result.is_err());
+}
+
+#[test]
+fn should_fail_if_evm_address_not_h160() {
+    // changed length of from address
+    let evm_tx_request_json = r#"{
+        "from": "0x1923f8dc025849e00f99c25fe2b2f7fb0db",
+        "gas": "0x55555",
+        "maxFeePerGas": "0x1234",
+        "maxPriorityFeePerGas": "0x1234",
+        "input": "0xabcd",
+        "nonce": "0x0",
+        "to": "0x07a565b7ed7d7a678680a4c162885bedbb695fe0",
+        "value": "0x1234"
+    }"#;
+
+    let basic_tx_result = parse_tx_request_json::<EVM>(evm_tx_request_json.to_string());
+
+    assert!(basic_tx_result.is_err());
+
+    println!("{:?}", basic_tx_result);
 }
