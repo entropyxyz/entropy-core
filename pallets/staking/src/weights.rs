22a2919d0e0a2a584fe6319ba776f2c10694c8ed
JesseAbram <33698952+JesseAbram@users.noreply.github.com>
2022-12-14 16:31:50 -0500

    add is syncing in  (#254)
    * add in on chain elements
    
    * fix benchamark script
    
    * extra substrate functions
    
    * fmt
    
    * fix test
    
    * clippy ignore weights
    
    * comments
    
    * clippy all
    
    * fix clippy
    
    * clippy
    
    * fmt

--- pallets/staking/src/weights.rs
+++ pallets/staking/src/weights.rs
@@ -1,5 +1,6 @@
 #![allow(unused_parens)]
 #![allow(unused_imports)]
+#![allow(clippy::all)]
 
 use frame_support::{
     traits::Get,
@@ -13,6 +14,7 @@ pub trait WeightInfo {
     fn change_threshold_accounts() -> Weight;
     fn withdraw_unbonded() -> Weight;
     fn validate() -> Weight;
+    fn declare_synced() -> Weight;
 }
 
 /// Weights for pallet_realyer using the Substrate node and recommended hardware.
@@ -60,6 +62,12 @@ impl<T: frame_system::Config> WeightInfo for SubstrateWeight<T> {
             .saturating_add(T::DbWeight::get().reads(11_u64))
             .saturating_add(T::DbWeight::get().writes(7_u64))
     }
+
+    fn declare_synced() -> Weight {
+        Weight::from_ref_time(36_000_000 as u64)
+            .saturating_add(T::DbWeight::get().reads(1 as u64))
+            .saturating_add(T::DbWeight::get().writes(1 as u64))
+    }
 }
 
 // For backwards compatibility and tests
@@ -105,5 +113,11 @@ impl WeightInfo for () {
         Weight::from_ref_time(95_000_000_u64)
             .saturating_add(RocksDbWeight::get().reads(11_u64))
             .saturating_add(RocksDbWeight::get().writes(7_u64))
+    }
+
+    fn declare_synced() -> Weight {
+        Weight::from_ref_time(36_000_000 as u64)
+            .saturating_add(RocksDbWeight::get().reads(1 as u64))
+            .saturating_add(RocksDbWeight::get().writes(1 as u64))
     }
 }
