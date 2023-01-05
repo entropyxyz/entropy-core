d9a431b52e544125edadb83a3a88b77aa6df1b03
JesseAbram <33698952+JesseAbram@users.noreply.github.com>
2023-01-05 15:37:18 -0500

    handle staking pallet errors better (#258)
    * handle staking pallet errors better
    
    * lint
    
    * clippy

--- node/cli/src/chain_spec.rs
+++ node/cli/src/chain_spec.rs
@@ -275,7 +275,6 @@ pub fn testnet_genesis(
             let nominations = initial_authorities
                 .as_slice()
                 .choose_multiple(&mut rng, count)
-                .into_iter()
                 .map(|choice| choice.0.clone())
                 .collect::<Vec<_>>();
             (x.clone(), x.clone(), STASH, StakerStatus::Nominator(nominations))
