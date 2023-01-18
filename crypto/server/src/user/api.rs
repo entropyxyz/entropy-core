0df26776304c2b198eee6607e1a0e5af6cff5a4e
JesseAbram <33698952+JesseAbram@users.noreply.github.com>
2023-01-18 10:43:58 -0500

    Swap key (#262)
    * swap key
    
    * tweak
    
    * tests working
    
    * spelling
    
    * fmt
    
    * refactor

--- crypto/server/src/user/api.rs
+++ crypto/server/src/user/api.rs
@@ -80,18 +80,18 @@ pub async fn new_user(
     let signer = get_signer(state).await?;
     // Checks if the user has registered onchain first.
     let key = signed_msg.account_id();
-    let is_registering = is_registering(&api, &key).await?;
-    if !is_registering {
-        return Err(UserErr::NotRegistering("Register Onchain first"));
-    }
+    let is_swapping = register_info(&api, &key).await?;
 
     let decrypted_message = signed_msg.decrypt(signer.signer());
     match decrypted_message {
         Ok(v) => {
-            // store new user data in kvdb
+            // store new user data in kvdb or deletes and replaces it if swapping
             let subgroup = get_subgroup(&api, &signer)
                 .await?
                 .ok_or_else(|| UserErr::SubgroupError("Subgroup Error"))?;
+            if is_swapping {
+                state.kv().delete(&key.to_string()).await?;
+            }
             let reservation = state.kv().reserve_key(key.to_string()).await?;
             state.kv().put(reservation, v).await?;
             // TODO: Error handling really complex needs to be thought about.
@@ -103,16 +103,22 @@ pub async fn new_user(
     }
     Ok(Status::Ok)
 }
-
-pub async fn is_registering(
+/// Returns wether an account is registering or swapping. If it is not, it returns error
+pub async fn register_info(
     api: &OnlineClient<EntropyConfig>,
     who: &AccountId32,
 ) -> Result<bool, UserErr> {
-    let is_registering_query = entropy::storage().relayer().registering(who);
-    let is_registering = api.storage().fetch(&is_registering_query, None).await?;
-    Ok(is_registering
-        .ok_or_else(|| UserErr::NotRegistering("Register Onchain first"))?
-        .is_registering)
+    let registering_info_query = entropy::storage().relayer().registering(who);
+    let register_info = api
+        .storage()
+        .fetch(&registering_info_query, None)
+        .await?
+        .ok_or_else(|| UserErr::NotRegistering("Register Onchain first"))?;
+    if !register_info.is_swapping && !register_info.is_registering {
+        return Err(UserErr::NotRegistering("Declare swap Onchain first"));
+    }
+
+    Ok(register_info.is_swapping)
 }
 
 // Returns PairSigner for this nodes threshold server.
@@ -148,7 +154,6 @@ pub async fn get_subgroup(
         .fetch(&stash_address_query, None)
         .await?
         .ok_or_else(|| UserErr::SubgroupError("Stash Fetch Error"))?;
-    // TODO: stash keys are broken up into subgroups....need to get stash key here from threshold
     for i in 0..SIGNING_PARTY_SIZE {
         let signing_group_addresses_query =
             entropy::storage().staking_extension().signing_groups(i as u8);
