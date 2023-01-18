da8162205921b5ba6654210fa07c7f4e1c2dd23d
jakehemmerle <jakehemmerle@protonmail.com>
2023-01-18 11:31:55 -0500

    fixed import

--- pallets/constraints/src/benchmarking.rs
+++ pallets/constraints/src/benchmarking.rs
@@ -7,6 +7,7 @@ use super::*;
 use crate::pallet::{Acl, Constraints, H160, H256};
 #[allow(unused)]
 use crate::Pallet as ConstraintsPallet;
+use sp_std::vec::Vec;
 
 fn assert_last_event<T: Config>(generic_event: <T as Config>::RuntimeEvent) {
     let events = frame_system::Pallet::<T>::events();
