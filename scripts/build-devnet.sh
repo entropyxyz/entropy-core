341ddde1f19df227c2ba42f9354c5df9ba6811fd
JesseAbram <jesseabramowitz@hotmail.com>
2023-02-01 12:22:49 -0500

    added script docs

--- scripts/build-devnet.sh
+++ scripts/build-devnet.sh
@@ -1,1 +1,2 @@
+# This script is meant to be run once before launching the devnet to build the inital chain spec
 ./target/release/entropy build-spec --disable-default-bootnode --raw --chain=devnet > chains/devnet.json
