349f13719ebb190c16a14c333ab192b2faf79fb5
John Sahhar <john@entropy.xyz>
2023-02-07 12:20:06 -0500

    server build working

--- scripts/build-and-upload.sh
+++ scripts/build-and-upload.sh
@@ -4,12 +4,14 @@ readonly ARCH=${1:-""}
 if [ ${#ARCH} -eq 0 ]; then echo "script needs arg" && exit 1; fi
 readonly tag="$(git tag|head -n 1)-$(git rev-parse --short HEAD)"
 readonly fn="$ARCH-$tag"
+readonly tar="entropy-$fn.tar.zst"
 rustup show
 rustup target add $ARCH
 rustup target add wasm32-unknown-unknown
-# cargo build -p entropy --target wasm32-unknown-unknown --release
+#cargo build -p entropy --target wasm32-unknown-unknown --release
 cargo build -p server --target $ARCH --release
-cp -r target/release "$fn"
-tar acvf "$fn.tar.zst" "$fn"
-_url="$(echo curl -sS -F\'file=@$fn.tar.zst\' 'https://entropy.family/u' | bash)"
-echo $_url
+touch $tar
+mkdir -p $fn
+mv 'target/release/entropy' 'target/release/server' $fn
+tar -acvf "$tar" "$fn"
+echo curl -sS -F\'file=@$tar\' 'https://entropy.family/u' | bash
