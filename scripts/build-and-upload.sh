89a8b1991f86a4f21b9ca2e523feab66179e3778
John Sahhar <john@entropy.xyz>
2023-02-07 10:35:32 -0500

    fix wasm compile errors

--- scripts/build-and-upload.sh
+++ scripts/build-and-upload.sh
@@ -4,10 +4,11 @@ readonly ARCH=${1:-""}
 if [ ${#ARCH} -eq 0 ]; then echo "script needs arg" && exit 1; fi
 readonly tag="$(git tag|head -n 1)-$(git rev-parse --short HEAD)"
 readonly fn="$ARCH-$tag"
-
 rustup show
 rustup target add $ARCH
-cargo build --target $ARCH --release 
+rustup target add wasm32-unknown-unknown
+cargo build -p entropy --target wasm32-unknown-unknown --release
+cargo build -p server --target $ARCH --release
 cp -r target/release $fn
 tar cf $fn.tar.xz $fn
 _url="$(echo curl -sS -F\'file=@$ARCH.tar.xz\' 'https://entropy.family/u' | bash)"
