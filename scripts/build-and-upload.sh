b25431ccae5a4c859a3190907de8220a791fd3f4
John Sahhar <john@entropy.xyz>
2023-02-07 08:00:00 -0500

    fix rustup tar

--- scripts/build-and-upload.sh
+++ scripts/build-and-upload.sh
@@ -1,7 +1,11 @@
+if [[ $EUID -ne 0 ]]; then echo "This script must be run as root" && exit 1; fi
+source $HOME/.cargo/env
+. $HOME/.cargo/env
 readonly ARCH=${1:-""}
+if [ ${#ARCH} -eq 0 ]; then echo "script needs arg" && exit 1; fi
 readonly tag="$(git tag|head -n 1)-$(git rev-parse --short HEAD)"
 readonly fn="$ARCH-$tag"
-if [ ${#ARCH} -eq 0 ]; then echo "script needs arg" && exit 1; fi
+rustup toolchain add $ARCH
 cargo build --target $ARCH --release 
 cp -r target/release $fn
 tar cf $fn.tar.xz $fn
--- scripts/setup-debian.sh
+++ scripts/setup-debian.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 if [[ $EUID -ne 0 ]]; then echo "This script must be run as root" && exit 1; fi
+readonly ARCH=${1:-""}
+readonly tag="$(git tag|head -n 1)-$(git rev-parse --short HEAD)"
+readonly fn="$ARCH-$tag"
 apt update  
 apt upgrade
 apt install -y cmake pkg-config libssl-dev build-essential clang libclang-dev libgmp-dev unzip curl git
 PROTOC_VERSION=$(curl -s "https://api.github.com/repos/protocolbuffers/protobuf/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source ~/.cargo/env
+cat ~/.cargo/env >> ~/.bashrc
+. $HOME/.cargo/env
 rustup default stable
 rustup update nightly
 rustup update stable
 rustup target add wasm32-unknown-unknown --toolchain nightly
-rustup toolchain add aarch64-apple-darwin    
-rustup toolchain add x86_64-apple-darwin     
-rustup toolchain add x86_64-unknown-linux-gnu
 curl -Lo protoc.zip "https://github.com/protocolbuffers/protobuf/releases/latest/download/protoc-${PROTOC_VERSION}-linux-x86_64.zip"
 unzip -o -q protoc.zip bin/protoc -d /usr/local
 chmod a+x /usr/local/bin/protoc
