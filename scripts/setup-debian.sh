8c25effdac0348917383da13a822015431f2b0b1
John Sahhar <john@entropy.xyz>
2023-02-07 09:28:56 -0500

    switch off gh rust

--- scripts/setup-debian.sh
+++ scripts/setup-debian.sh
@@ -17,13 +17,16 @@ chmod a+x /usr/local/bin/protoc
 
 # cargo
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source ~/.cargo/env
-cat ~/.cargo/env >> ~/.bashrc
-. $HOME/.cargo/env
-rustup default stable
-rustup target add x86_64-unknown-linux-gnu --toolchain stable
-rustup target add x86_64-apple-darwin --toolchain stable
-rustup target add aarch64-apple-darwin --toolchain stable
+source $HOME/.cargo/env
+#cat ~/.cargo/env >> ~/.bashrc
+#. $HOME/.cargo/env
+rustup set profile complete
 rustup target add wasm32-unknown-unknown --toolchain nightly
+rustup default stable
+#rustup target add x86_64-unknown-linux-gnu --toolchain stable
+#rustup target add x86_64-apple-darwin --toolchain stable
+#rustup target add aarch64-apple-darwin --toolchain stable
 rustup update stable
 rustup update nightly
+
+#. $HOME/.cargo/env
