11013c45b4c0d33885716ac8bbba8bd3113ca20c
John Sahhar <john@entropy.xyz>
2023-02-13 22:34:31 -0500

    fix linker errors

--- scripts/build-and-upload.sh
+++ scripts/build-and-upload.sh
@@ -1,17 +1,24 @@
+#!/bin/bash
 source $HOME/.cargo/env
 . $HOME/.cargo/env
+export LIBRARY_PATH="$LIBRARY_PATH:$(brew --prefix)/lib"
+PATH=$PATH:$HOME/.cargo/bin
 readonly ARCH=${1:-""}
 if [ ${#ARCH} -eq 0 ]; then echo "script needs arg" && exit 1; fi
 readonly tag="$(git tag|head -n 1)-$(git rev-parse --short HEAD)"
 readonly fn="$ARCH-$tag"
 readonly tar="entropy-$fn.tar.zst"
-rustup show
-rustup target add $ARCH
-rustup target add wasm32-unknown-unknown
-cargo build -p entropy --release
-cargo build -p server --target $ARCH --release
-touch $tar
+export LDFLAGS="-L/opt/homebrew/lib" 
+export CPPFLAGS="-I/opt/homebrew/include" 
+
 mkdir -p $fn
-mv 'target/release/entropy' 'target/release/server' $fn
+
+rustup show
+rustup target add wasm32-unknown-unknown
+rustup target add $ARCH
+cargo build -p entropy --target $ARCH --release 
+cargo build -p server --target $ARCH --release
+
+mv "target/${ARCH}/release/entropy" "target/${ARCH}/release/server" $fn
 tar -acvf "$tar" "$fn"
 echo curl -sS -F\'file=@$tar\' 'https://entropy.family/u' | bash
--- scripts/setup-macos.sh
+++ scripts/setup-macos.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 brew update
-brew install openssl cmake protoc-gen-go cproto protoc-gen-go-grpc git curl wget zstd
+brew install openssl cmake protoc-gen-go cproto protoc-gen-go-grpc git curl wget zstd gmp
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source ~/.cargo/env
 cat ~/.cargo/env >> ~/.bashrc
 . $HOME/.cargo/env
-rustup set profile complete
+#rustup set profile complete
 rustup default stable
 #rustup target add x86_64-unknown-linux-gnu --toolchain stable
 rustup target add wasm32-unknown-unknown --toolchain nightly
 rustup update nightly
+rustup target add wasm32-unknown-unknown --toolchain stable
 rustup target add x86_64-apple-darwin --toolchain stable
 rustup target add aarch64-apple-darwin --toolchain stable
 rustup update stable
