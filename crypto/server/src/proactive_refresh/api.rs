7820257c8b4634ad356605004cc5c53a1a8a85f4
JesseAbram <jesseabramowitz@hotmail.com>
2023-03-21 13:02:13 -0400

    is node requested function

--- crypto/server/src/proactive_refresh/api.rs
+++ crypto/server/src/proactive_refresh/api.rs
@@ -7,10 +7,11 @@ use parity_scale_codec::{Decode, Encode};
 use rocket::{http::Status, response::stream::EventStream, serde::json::Json, Shutdown, State};
 use subxt::OnlineClient;
 use tracing::instrument;
+use sp_core::crypto::AccountId32;
 
 use crate::{
     chain_api::{entropy, get_api, EntropyConfig},
-    helpers::signing::SignatureState,
+    helpers::validator::get_signer,
     Configuration,
 	proactive_refresh::errors::RefreshErr
 };
@@ -21,7 +22,9 @@ pub async fn refresh(
     kv: &State<KvManager>,
     config: &State<Configuration>,
 ) -> Result<Status, RefreshErr> {
-	let data = RefreshMessages::decode(&mut encoded_data.as_ref());
+	let data = RefreshMessages::decode(&mut encoded_data.as_ref()).unwrap();
+	let node_account = get_signer(&kv).await.unwrap();
+	is_node_in_request(node_account.account_id(), &data).unwrap();
 	Ok(Status::Ok)
 }
 
@@ -31,6 +34,14 @@ pub fn validate_proactive_refresh() {
 }
 
 
-pub fn is_node_in_request() {
+pub fn is_node_in_request(node_address: &AccountId32, data: &RefreshMessages) -> Result<(), ()> {
+	let validator_addresses = data.iter().map(|refresh_message| {
+		let converted_address = refresh_message.validator_account.clone().try_into().expect("slice with incorrect length");
+		AccountId32::new(converted_address)
+	}).collect::<Vec<AccountId32>>();
 
+	if !validator_addresses.contains(node_address) {
+		return Err(())
+	}
+	Ok(())
 }
--- crypto/server/src/proactive_refresh/tests.rs
+++ crypto/server/src/proactive_refresh/tests.rs
@@ -1,5 +1,6 @@
 use entropy_constraints::{Architecture, Evm, Parse};
 use entropy_shared::{Message, OCWMessage, RefreshMessages, RefreshMessage};
+use bip39::{Language, Mnemonic};
 use kvdb::clean_tests;
 use parity_scale_codec::Encode;
 use rocket::http::{ContentType, Status};
@@ -7,11 +8,13 @@ use serial_test::serial;
 use sp_keyring::{AccountKeyring, Sr25519Keyring};
 use subxt::{tx::PairSigner, OnlineClient};
 use testing_utils::context::test_context_stationary;
+use sp_core::{crypto::AccountId32, Pair};
 
 use crate::{
     chain_api::{entropy, get_api, EntropyConfig},
-    helpers::tests::setup_client,
+    helpers::{tests::setup_client, launch::{DEFAULT_MNEMONIC, DEFAULT_BOB_MNEMONIC}},
     r#unsafe::api::UnsafeQuery,
+	message::mnemonic_to_pair,
 };
 
 #[rocket::async_test]
@@ -19,13 +22,18 @@ use crate::{
 async fn test_new_refresh() {
     clean_tests();
     let client = setup_client().await;
+	let alice_pair = mnemonic_to_pair(&Mnemonic::from_phrase(DEFAULT_MNEMONIC, Language::English).unwrap());
+	let alice_address = AccountId32::new(alice_pair.public().0);
+	let bob_pair = mnemonic_to_pair(&Mnemonic::from_phrase(DEFAULT_BOB_MNEMONIC, Language::English).unwrap());
+	let bob_address = AccountId32::new(bob_pair.public().0);
+
 	let mock_refresh_message_alice = RefreshMessage {
-		validator_ip: b"127.0.0.1:3001".to_vec(),
-		validator_account: b"5H8qc7f4mXFY16NBWSB9qkc6pTks98HdVuoQTs1aova5fRtN".to_vec()
+		validator_ip: b"127.0.0.1:3001".encode(),
+		validator_account: alice_address.encode()
 	};
 	let mock_refresh_message_bob = RefreshMessage {
-		validator_ip: b"127.0.0.1:3002".to_vec(),
-		validator_account: b"5D2SVCUkK5FgFiBwPTJuTN65J6fACSEoZrL41thZBAycwnQV".to_vec()
+		validator_ip: b"127.0.0.1:3002".encode(),
+		validator_account: bob_address.encode()
 	};
 	let mock_refresh_messages: RefreshMessages = vec![mock_refresh_message_alice, mock_refresh_message_bob];
 
