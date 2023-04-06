2a272c4c63a1f9d5f41e451fcab0945dee05c7e1
ok-john <john@entropy.xyz>
2023-04-06 15:23:39 -0400

    Add cors headers (#315)

--- crypto/server/src/main.rs
+++ crypto/server/src/main.rs
@@ -22,6 +22,11 @@ mod signing_client;
 mod r#unsafe;
 mod user;
 mod validator;
+use rocket::{
+    fairing::{Fairing, Info, Kind},
+    http::Header,
+    Request, Response,
+};
 use validator::api::get_random_server_info;
 
 #[macro_use]
@@ -49,6 +54,21 @@ use crate::{
         tell_chain_syncing_is_done,
     },
 };
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info { Info { name: "Add CORS headers to responses", kind: Kind::Response } }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response
+            .set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 #[launch]
 async fn rocket() -> _ {
@@ -123,4 +143,5 @@ async fn rocket() -> _ {
         .manage(signature_state)
         .manage(configuration)
         .manage(kv_store)
+        .attach(CORS)
 }
