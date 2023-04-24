158cec83f38fe1055fcac0ad759dda995ba1f3d0
jakehemmerle <jakehemmerle@protonmail.com>
2023-04-24 12:18:25 -0700

    fdsa

--- crypto/server/src/chain_api.rs
+++ crypto/server/src/chain_api.rs
@@ -4,14 +4,14 @@ use subxt::OnlineClient;
 
 #[subxt::subxt(runtime_metadata_path = "entropy_metadata.scale")]
 pub mod entropy {
+    #[subxt::subxt(substitute_type = "entropy::shared")]
+    use ::entropy_shared;
     #[subxt::subxt(substitute_type = "entropy_shared::constraints::acl::Acl")]
     use ::entropy_shared::Acl;
     #[subxt::subxt(substitute_type = "entropy_shared::constraints::Constraints")]
     use ::entropy_shared::Constraints;
     #[subxt::subxt(substitute_type = "sp_core::crypto::AccountId32")]
     use ::sp_core::crypto::AccountId32;
-    #[subxt::subxt(substitute_type = "entropy::shared")]
-    use ::entropy_shared;
 }
 
 /// Creates an api instance to talk to chain
