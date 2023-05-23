9fa71778dd671e3eced4fc47b70c51c9131c527d
jesse <jesse@entropy.wxy>
2023-05-23 14:29:39 -0400

    proper metadata

--- scripts/pull_entropy_metadata.sh
+++ scripts/pull_entropy_metadata.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-curl -sX POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"state_getMetadata", "id": 1}' localhost:9933 \
-                     | jq .result \
-                     | cut -d '"' -f 2 \
-                     | xxd -r -p > ./crypto/server/entropy_metadata.scale
+subxt metadata -f bytes > ./crypto/server/entropy_metadata.scale
+
+
