d99a8706a8129536b38e801c6d4c33d54957faca
peg <ameba23@systemli.org>
2023-06-12 09:08:45 +0200

    Add a script to create typeshare definitions for typescript

---
+++ scripts/typeshare.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Create typeshare shared type definitions for typescript (for the SDK)
+# Requires typeshare-cli to be installed (`cargo install typeshare-cli`)
+# This script must be run from the root directory of `entropy-core`
+typeshare ./crypto/server --lang=typescript --output-file=shared-types-sdk.ts
