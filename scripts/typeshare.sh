ce9a1b84f3bcebd6c16fe85f567d74c75a5fc9a7
peg <ameba23@systemli.org>
2023-06-12 09:30:42 +0200

    Update script to build typeshare interfaces

--- scripts/typeshare.sh
+++ scripts/typeshare.sh
@@ -2,4 +2,4 @@
 # Create typeshare shared type definitions for typescript (for the SDK)
 # Requires typeshare-cli to be installed (`cargo install typeshare-cli`)
 # This script must be run from the root directory of `entropy-core`
-typeshare ./crypto/server --lang=typescript --output-file=shared-types-sdk.ts
+typeshare ./crypto/server ./crypto/shared --lang=typescript --output-file=shared-types-sdk.ts
