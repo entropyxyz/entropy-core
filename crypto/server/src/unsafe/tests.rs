533a71140d435d8602dd541e8923452051a29e38
jesse <jesse@entropy.wxy>
2023-06-14 13:13:30 -0400

    unsafe tests

--- crypto/server/src/unsafe/tests.rs
+++ crypto/server/src/unsafe/tests.rs
@@ -1,3 +1,4 @@
+use axum::http::StatusCode;
 use kvdb::clean_tests;
 use rocket::http::{ContentType, Status};
 use serial_test::serial;
@@ -8,37 +9,46 @@ use crate::helpers::tests::setup_client;
 #[rocket::async_test]
 #[serial]
 async fn test_unsafe_get_endpoint() {
-    let client = setup_client().await;
+    setup_client().await;
+    let client = reqwest::Client::new();
+
     let get_query = UnsafeQuery::new("MNEMONIC".to_string(), "foo".to_string()).to_json();
 
     // Test that the get endpoint works
     let response = client
-        .post("/unsafe/get")
-        .header(ContentType::JSON)
+        .post("http://localhost:3001/unsafe/get")
+        .header("Content-Type", "application/json")
         .body(get_query.clone())
-        .dispatch()
-        .await;
+        .send()
+        .await
+        .unwrap();
 
-    assert_eq!(response.status(), Status::Ok);
-    let response_mnemonic = response.into_string().await.unwrap();
+    assert_eq!(response.status(), StatusCode::OK);
+    let response_mnemonic = response.text().await.unwrap();
     assert!(!response_mnemonic.is_empty());
 
     // Update the mnemonic, testing the put endpoint works
     let put_response = client
-        .post("/unsafe/put")
-        .header(ContentType::JSON)
+        .post("http://localhost:3001/unsafe/put")
+        .header("Content-Type", "application/json")
         .body(get_query.clone())
-        .dispatch()
-        .await;
+        .send()
+        .await
+        .unwrap();
 
-    assert_eq!(put_response.status(), Status::Ok);
+    assert_eq!(put_response.status(), StatusCode::OK);
 
     // Check the updated mnemonic is the new value
-    let get_response =
-        client.post("/unsafe/get").header(ContentType::JSON).body(get_query).dispatch().await;
+    let get_response = client
+        .post("http://localhost:3001/unsafe/get")
+        .header("Content-Type", "application/json")
+        .body(get_query)
+        .send()
+        .await
+        .unwrap();
 
-    assert_eq!(get_response.status(), Status::Ok);
-    let updated_response_mnemonic = get_response.into_string().await.unwrap();
+    assert_eq!(get_response.status(), StatusCode::OK);
+    let updated_response_mnemonic = get_response.text().await.unwrap();
     assert_eq!(updated_response_mnemonic, "foo".to_string());
 
     clean_tests();
