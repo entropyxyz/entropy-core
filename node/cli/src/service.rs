498bac8ad122ed93b9d1879dc04ffe1e2ac71ef7
jesse <jesse@entropy.wxy>
2023-07-13 17:38:20 -0400

    fix redundant clone bug

--- node/cli/src/service.rs
+++ node/cli/src/service.rs
@@ -195,6 +195,7 @@ pub fn new_partial(
 
     let (grandpa_block_import, grandpa_link) = grandpa::block_import(
         client.clone(),
+        #[allow(clippy::redundant_clone)]
         &(client.clone() as Arc<_>),
         select_chain.clone(),
         telemetry.as_ref().map(|x| x.handle()),
