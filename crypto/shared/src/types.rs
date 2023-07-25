ded6e98e72005b870c71e48d5bfd05f2372fc669
jesse <jesse@entropy.wxy>
2023-07-25 07:42:43 -0400

    comments fix

--- crypto/shared/src/types.rs
+++ crypto/shared/src/types.rs
@@ -10,10 +10,10 @@ pub use crate::constraints::*;
 /// interactions with the threshold server (eg distributing threshold shares).
 pub type X25519PublicKey = [u8; 32];
 
-// Defines a user's key visibility
-// Public -> User does not hold a key shard and anyone can ask for a signature
-// Permissioned -> User done not hold a key shard but only they can ask for a signature
-// Private -> User holds a key shard and only they can ask for a signature
+/// Defines an application's accessibility
+/// Public -> Anyone can request a signature
+/// Permissioned -> Only permissioned users can request a signature
+/// Private -> Requires the keyshare holder to participate in the threshold signing process
 #[derive(Copy, Clone, PartialEq, Eq, RuntimeDebug, Encode, Decode, TypeInfo, MaxEncodedLen)]
 pub enum KeyVisibility {
     Public,
