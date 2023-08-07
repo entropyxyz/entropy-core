c62a6cf9b002c7f10284276215ed24bb6a079280
jesse <jesse@entropy.wxy>
2023-08-07 14:57:14 -0400

    setup listener for dkg

--- crypto/server/src/helpers/user.rs
+++ crypto/server/src/helpers/user.rs
@@ -1,21 +1,33 @@
-use crate::user::errors::UserErr;
+use sp_core::crypto::AccountId32;
+use subxt::{
+    ext::sp_core::{sr25519, Pair},
+    tx::PairSigner,
+};
 
+use crate::{
+    chain_api::EntropyConfig,
+    signing_client::{
+        protocol_transport::{open_protocol_connections, Listener},
+        SignerState,
+    },
+    user::{api::ValidatorInfo, errors::UserErr},
+};
 /// complete the dkg process for a new user
-pub async fn do_dkg() -> Result<(), UserErr> {
-    // // subscribe to all other participating parties. Listener waits for other subscribers.
-    // let (rx_ready, rx_from_others, listener) = Listener::new(message, &account_sp_core);
-    // state
-    //     .listeners
-    //     .lock()
-    // 	.map_err(|_| SigningErr::SessionError("Error getting lock".to_string()))?
-    //     // TODO: using signature ID as session ID. Correct?
-    //     .insert(sign_context.sign_init.sig_uid.clone(), listener);
+pub async fn do_dkg(
+    validators_info: Vec<ValidatorInfo>,
+    signer: PairSigner<EntropyConfig, sr25519::Pair>,
+    state: &SignerState,
+    session_uid: String,
+) -> Result<(), UserErr> {
+    let account_sp_core = AccountId32::new(*signer.account_id().clone().as_ref());
+    // subscribe to all other participating parties. Listener waits for other subscribers.
+    let (rx_ready, rx_from_others, listener) = Listener::new(validators_info, &account_sp_core);
+    state
+	.listeners
+	.lock()
+	.map_err(|_| UserErr::SessionError("Error getting lock".to_string()))?
+	// TODO: using signature ID as session ID. Correct?
+	.insert(session_uid.clone(), listener);
 
-    // open_protocol_connections(&sign_context, &my_id, &signer, state).await?;
-    // let channels = {
-    //     let ready = timeout(Duration::from_secs(SETUP_TIMEOUT_SECONDS), rx_ready).await?;
-    //     let broadcast_out = ready??;
-    //     Channels(broadcast_out, rx_from_others)
-    // };
     Ok(())
 }
