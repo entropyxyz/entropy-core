b5c98d36c75bdeae1609ee768552ca0deed7a518
jesse <jesse@entropy.wxy>
2023-08-07 15:06:40 -0400

    integrate open protocol connection to dkg

--- crypto/server/src/helpers/user.rs
+++ crypto/server/src/helpers/user.rs
@@ -1,3 +1,4 @@
+use kvdb::kv_manager::{KvManager, PartyId};
 use sp_core::crypto::AccountId32;
 use subxt::{
     ext::sp_core::{sr25519, Pair},
@@ -15,19 +16,23 @@ use crate::{
 /// complete the dkg process for a new user
 pub async fn do_dkg(
     validators_info: Vec<ValidatorInfo>,
-    signer: PairSigner<EntropyConfig, sr25519::Pair>,
+    signer: &PairSigner<EntropyConfig, sr25519::Pair>,
     state: &SignerState,
     session_uid: String,
 ) -> Result<(), UserErr> {
     let account_sp_core = AccountId32::new(*signer.account_id().clone().as_ref());
     // subscribe to all other participating parties. Listener waits for other subscribers.
-    let (rx_ready, rx_from_others, listener) = Listener::new(validators_info, &account_sp_core);
+    let (rx_ready, rx_from_others, listener) = Listener::new(validators_info.clone(), &account_sp_core);
     state
 	.listeners
 	.lock()
 	.map_err(|_| UserErr::SessionError("Error getting lock".to_string()))?
 	// TODO: using signature ID as session ID. Correct?
 	.insert(session_uid.clone(), listener);
+
+    let my_id = PartyId::new(account_sp_core.clone());
+
+    open_protocol_connections(&validators_info, &session_uid, &my_id, &signer, state).await?;
 
     Ok(())
 }
