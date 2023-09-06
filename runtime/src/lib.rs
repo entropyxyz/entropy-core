f5e943c699b16fe879e5088d42ff1777fbaec90d
jesse <jesse@entropy.wxy>
2023-09-06 12:49:39 -0400

    fmt

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -125,6 +125,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     // and set impl_version to 0. If only runtime
     // implementation changes and behavior does not, then leave spec_version as
     // is and increment impl_version.
+    #[allow(clippy::zero_prefixed_literal)]
     spec_version: 005,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
