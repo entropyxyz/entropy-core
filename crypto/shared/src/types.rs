bf3c73387eac11e2db868e733e576bc33f8d561b
peg <ameba23@systemli.org>
2023-09-21 10:54:15 +0200

    Add wasm feature to entropy-shared

--- crypto/shared/src/types.rs
+++ crypto/shared/src/types.rs
@@ -1,8 +1,14 @@
 #![allow(dead_code)]
-use codec::{alloc::vec::Vec, Decode, Encode, MaxEncodedLen};
-use frame_support::RuntimeDebug;
-use node_primitives::BlockNumber;
+use codec::{Decode, Encode, MaxEncodedLen};
 use scale_info::TypeInfo;
+
+#[cfg(not(feature = "wasm"))]
+use codec::alloc::vec::Vec;
+#[cfg(not(feature = "wasm"))]
+use frame_support::RuntimeDebug;
+#[cfg(not(feature = "wasm"))]
+use node_primitives::BlockNumber;
+
 #[cfg(feature = "std")]
 use serde::{Deserialize, Serialize};
 
@@ -17,8 +23,10 @@ pub type X25519PublicKey = [u8; 32];
 /// Public -> Anyone can request a signature
 /// Permissioned -> Only permissioned users can request a signature
 /// Private -> Requires the keyshare holder to participate in the threshold signing process
+#[cfg_attr(feature = "wasm", derive(Debug))]
+#[cfg_attr(not(feature = "wasm"), derive(RuntimeDebug))]
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-#[derive(Copy, Clone, PartialEq, Eq, RuntimeDebug, Encode, Decode, TypeInfo, MaxEncodedLen)]
+#[derive(Copy, Clone, PartialEq, Eq, Encode, Decode, TypeInfo, MaxEncodedLen)]
 pub enum KeyVisibility {
     Public,
     Permissioned,
@@ -34,10 +42,11 @@ pub struct ValidatorInfo {
     pub tss_account: codec::alloc::vec::Vec<u8>,
 }
 
+#[cfg(not(feature = "wasm"))]
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[derive(Clone, Encode, Decode, Debug, Eq, PartialEq, TypeInfo)]
 pub struct OcwMessage {
     pub block_number: BlockNumber,
-    pub sig_request_accounts: Vec<codec::alloc::vec::Vec<u8>>,
+    pub sig_request_accounts: Vec<Vec<u8>>,
     pub validators_info: Vec<ValidatorInfo>,
 }
