6e94a05d1ff6764918a1ade95594d9789c183e9d
peg <ameba23@systemli.org>
2023-10-09 11:07:16 +0200

    Nodejs test

--- crypto/protocol/nodejs-test/index.js
+++ crypto/protocol/nodejs-test/index.js
@@ -1,11 +1,11 @@
-import { run_dkg_protocol } from "entropy-protocol"
-// import "websocket-polyfill"
+const protocol = require('entropy-protocol')
+Object.assign(global, { WebSocket: require('ws') });
 
 const unused = new Uint8Array(32)
 const userSigningKeypairSeed = new Uint8Array(32)
 const x25519PrivateKey = new Uint8Array(32)
 
-run_dkg_protocol(unused, userSigningKeypairSeed, x25519PrivateKey).then((output) => {
+protocol.run_dkg_protocol(unused, userSigningKeypairSeed, x25519PrivateKey).then((output) => {
   console.log('OUTPUT:', output)
 }).catch((err) => {
   console.log('ERR', err)
