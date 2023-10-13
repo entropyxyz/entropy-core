042dd332cb7ad68559793d4e938508a3159982e0
Hernando Castano <hernando@hcastano.com>
2023-10-13 11:30:24 -0400

    Place `demo_offence` dispatchable behind root origin check

--- pallets/slashing/src/tests.rs
+++ pallets/slashing/src/tests.rs
@@ -1,4 +1,4 @@
-use frame_support::assert_ok;
+use frame_support::{assert_ok, assert_err};
 use sp_runtime::Perbill;
 use sp_staking::offence::Offence;
 
@@ -18,13 +18,26 @@ fn offence_test() {
     new_test_ext().execute_with(|| {
         assert_ok!(Staking::force_new_era_always(RuntimeOrigin::root()));
         assert!(Session::validators().contains(&1));
+
         // slash would cause min validators to drop below min validators no offence
-        assert_ok!(Slashing::demo_offence(RuntimeOrigin::signed(1), vec![1u64, 2u64]));
+        assert_ok!(Slashing::demo_offence(RuntimeOrigin::root(), 1, vec![1u64, 2u64]));
         let mut offences = OFFENCES.with(|l| l.replace(vec![]));
         assert_eq!(offences.len(), 0);
+
         // causes offence
-        assert_ok!(Slashing::demo_offence(RuntimeOrigin::signed(1), vec![1u64]));
+        assert_ok!(Slashing::demo_offence(RuntimeOrigin::root(), 1, vec![1u64]));
         offences = OFFENCES.with(|l| l.replace(vec![]));
         assert_eq!(offences.len(), 1);
     });
 }
+
+#[test]
+fn signed_origin_cannot_initiate_demo_offence() {
+    new_test_ext().execute_with(|| {
+        assert_err!(
+            Slashing::demo_offence(RuntimeOrigin::signed(1), 1, vec![1u64, 2u64]),
+            sp_runtime::DispatchError::BadOrigin
+        );
+    })
+}
+
