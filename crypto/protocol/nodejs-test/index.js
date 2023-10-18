40086fc63f6aadd4651f69e3cd1dffa2563b4720
peg <ameba23@systemli.org>
2023-10-18 13:51:39 +0200

    WIP Update nodejs test

--- crypto/protocol/nodejs-test/index.js
+++ crypto/protocol/nodejs-test/index.js
@@ -1,11 +1,16 @@
 const protocol = require('entropy-protocol')
 Object.assign(global, { WebSocket: require('ws') });
 
-const unused = new Uint8Array(32)
+const validatorInfo = new protocol.ValidatorInfo(
+    new Uint8Array(32), // x25519PrivateKey
+    '127.0.0.1:3000',
+    new Uint8Array(32), // TSS Account
+)
+
 const userSigningKeypairSeed = new Uint8Array(32)
 const x25519PrivateKey = new Uint8Array(32)
 
-protocol.run_dkg_protocol(unused, userSigningKeypairSeed, x25519PrivateKey).then((output) => {
+protocol.run_dkg_protocol([validatorInfo], userSigningKeypairSeed, x25519PrivateKey).then((output) => {
   console.log('OUTPUT:', output)
 }).catch((err) => {
   console.log('ERR', err)
