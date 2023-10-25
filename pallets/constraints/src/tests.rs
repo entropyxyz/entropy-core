a0bbbe46698c610f89b010c92a18c32705176fef
Hernando Castano <hernando@hcastano.com>
2023-10-25 12:29:07 -0400

    Update constraint tests to use "program" wording

--- pallets/constraints/src/tests.rs
+++ pallets/constraints/src/tests.rs
@@ -4,75 +4,75 @@ use pallet_balances::Error as BalancesError;
 use crate::{mock::*, AllowedToModifyProgram, Error};
 
 /// consts used for testing
-const CONSTRAINT_ACCOUNT: u64 = 1u64;
+const PROGRAM_MODIFICATION_ACCOUNT: u64 = 1u64;
 const SIG_REQ_ACCOUNT: u64 = 2u64;
 
 #[test]
-fn set_constraints() {
+fn set_programs() {
     new_test_ext().execute_with(|| {
-        let empty_constraint = vec![];
-        let constraint = vec![10u8, 11u8];
+        let empty_program = vec![];
+        let program = vec![10u8, 11u8];
         let too_long = vec![1u8, 2u8, 3u8, 4u8, 5u8];
 
-        // make sure no one can add a constraint without explicit permissions
+        // make sure no one can add a program without explicit permissions
         assert_noop!(
             ConstraintsPallet::update_program(
-                RuntimeOrigin::signed(CONSTRAINT_ACCOUNT),
+                RuntimeOrigin::signed(PROGRAM_MODIFICATION_ACCOUNT),
                 SIG_REQ_ACCOUNT,
-                constraint.clone(),
+                program.clone(),
             ),
             Error::<Test>::NotAuthorized
         );
 
-        AllowedToModifyProgram::<Test>::insert(CONSTRAINT_ACCOUNT, SIG_REQ_ACCOUNT, ());
+        AllowedToModifyProgram::<Test>::insert(PROGRAM_MODIFICATION_ACCOUNT, SIG_REQ_ACCOUNT, ());
 
         // can't pay deposit
         assert_noop!(
             ConstraintsPallet::update_program(
-                RuntimeOrigin::signed(CONSTRAINT_ACCOUNT),
+                RuntimeOrigin::signed(PROGRAM_MODIFICATION_ACCOUNT),
                 SIG_REQ_ACCOUNT,
-                constraint.clone(),
+                program.clone(),
             ),
             BalancesError::<Test>::InsufficientBalance
         );
 
-        Balances::make_free_balance_be(&CONSTRAINT_ACCOUNT, 100);
+        Balances::make_free_balance_be(&PROGRAM_MODIFICATION_ACCOUNT, 100);
 
-        // It's okay to have an empty constraint
+        // It's okay to have an empty program
         assert_ok!(ConstraintsPallet::update_program(
-            RuntimeOrigin::signed(CONSTRAINT_ACCOUNT),
+            RuntimeOrigin::signed(PROGRAM_MODIFICATION_ACCOUNT),
             SIG_REQ_ACCOUNT,
-            empty_constraint.clone()
+            empty_program.clone()
         ));
 
         assert_ok!(ConstraintsPallet::update_program(
-            RuntimeOrigin::signed(CONSTRAINT_ACCOUNT),
+            RuntimeOrigin::signed(PROGRAM_MODIFICATION_ACCOUNT),
             SIG_REQ_ACCOUNT,
-            constraint.clone()
+            program.clone()
         ));
 
-        assert_eq!(ConstraintsPallet::bytecode(SIG_REQ_ACCOUNT).unwrap(), constraint);
-        assert_eq!(Balances::free_balance(CONSTRAINT_ACCOUNT), 90);
+        assert_eq!(ConstraintsPallet::bytecode(SIG_REQ_ACCOUNT).unwrap(), program);
+        assert_eq!(Balances::free_balance(PROGRAM_MODIFICATION_ACCOUNT), 90);
 
         // deposit refunded partial
         assert_ok!(ConstraintsPallet::update_program(
-            RuntimeOrigin::signed(CONSTRAINT_ACCOUNT),
+            RuntimeOrigin::signed(PROGRAM_MODIFICATION_ACCOUNT),
             SIG_REQ_ACCOUNT,
             vec![10u8]
         ));
-        assert_eq!(Balances::free_balance(CONSTRAINT_ACCOUNT), 95);
+        assert_eq!(Balances::free_balance(PROGRAM_MODIFICATION_ACCOUNT), 95);
 
         // deposit refunded full
         assert_ok!(ConstraintsPallet::update_program(
-            RuntimeOrigin::signed(CONSTRAINT_ACCOUNT),
+            RuntimeOrigin::signed(PROGRAM_MODIFICATION_ACCOUNT),
             SIG_REQ_ACCOUNT,
             vec![]
         ));
-        assert_eq!(Balances::free_balance(CONSTRAINT_ACCOUNT), 100);
+        assert_eq!(Balances::free_balance(PROGRAM_MODIFICATION_ACCOUNT), 100);
 
         assert_noop!(
             ConstraintsPallet::update_program(
-                RuntimeOrigin::signed(CONSTRAINT_ACCOUNT),
+                RuntimeOrigin::signed(PROGRAM_MODIFICATION_ACCOUNT),
                 SIG_REQ_ACCOUNT,
                 too_long,
             ),
--- pallets/relayer/src/tests.rs
+++ pallets/relayer/src/tests.rs
@@ -16,7 +16,7 @@ use crate as pallet_relayer;
 use crate::{mock::*, Error, RegisteredInfo, RegisteringDetails, ValidateConfirmRegistered};
 
 /// consts used for testing
-const CONSTRAINT_ACCOUNT: u64 = 1u64;
+const PROGRAM_MODIFICATION_ACCOUNT: u64 = 1u64;
 const SIG_REQ_ACCOUNT: u64 = 2u64;
 
 #[test]
@@ -78,11 +78,11 @@ fn it_takes_a_program_storage_deposit_during_register() {
         let program = vec![1u8, 2u8];
         let initial_balance = 100;
 
-        Balances::make_free_balance_be(&CONSTRAINT_ACCOUNT, initial_balance);
+        Balances::make_free_balance_be(&PROGRAM_MODIFICATION_ACCOUNT, initial_balance);
 
         assert_ok!(Relayer::register(
             RuntimeOrigin::signed(SIG_REQ_ACCOUNT),
-            CONSTRAINT_ACCOUNT,
+            PROGRAM_MODIFICATION_ACCOUNT,
             KeyVisibility::Public,
             program.clone(),
         ));
@@ -91,7 +91,7 @@ fn it_takes_a_program_storage_deposit_during_register() {
             * (program.len() as u32);
 
         assert_eq!(
-            Balances::free_balance(CONSTRAINT_ACCOUNT),
+            Balances::free_balance(PROGRAM_MODIFICATION_ACCOUNT),
             initial_balance - (expected_reserve as u64)
         );
     });
