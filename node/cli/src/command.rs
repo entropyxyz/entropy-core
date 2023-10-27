5d4c43c8ecfa3bfeaed8d09287487d44746a5dfb
Hernando Castano <hernando@hcastano.com>
2023-10-27 17:01:09 -0400

    Update some of the CLI fields

--- node/cli/src/command.rs
+++ node/cli/src/command.rs
@@ -13,7 +13,7 @@ use crate::{
 
 impl SubstrateCli for Cli {
     fn impl_name() -> String {
-        "Substrate Node".into()
+        "Entropy Node".into()
     }
 
     fn impl_version() -> String {
@@ -29,11 +29,11 @@ impl SubstrateCli for Cli {
     }
 
     fn support_url() -> String {
-        "support.anonymous.an".into()
+        "https://github.com/entropyxyz/entropy-core/".into()
     }
 
     fn copyright_start_year() -> i32 {
-        2017
+        2022
     }
 
     fn load_spec(&self, id: &str) -> Result<Box<dyn sc_service::ChainSpec>, String> {
