c094ddce56236ecce04f386a19a05f525d406ade
peg <ameba23@systemli.org>
2023-11-09 10:33:48 +0100

    Tidy nodejs-test

--- crypto/protocol/nodejs-test/index.js
+++ crypto/protocol/nodejs-test/index.js
@@ -1,8 +1,11 @@
 // This takes parameters for the sign or dkg protocols as a JSON serialized string given as a
 // command line argument
-//
-// It is called from a rust test in `server` by spawning a process running Nodejs with this script
+
+// It can be called from a rust test in `server` by spawning a process running Nodejs with this script
 const protocol = require('entropy-protocol')
+
+// This is needed on Nodejs as we use bindings to the browser websocket API which is a property
+// of the global object
 Object.assign(global, { WebSocket: require('ws') });
 
 // Create ValidatorInfo objects from objects parsed from JSON
@@ -26,21 +29,21 @@ try {
 } catch (err) {
     console.log(`Usage: ${process.argv[0]} <command> <JSON payload>`)
     console.log(err)
+    process.exit(1)
 }
 
 switch(process.argv[2].toLowerCase()) {
     case 'register':
-        console.log('Starting DKG protocol with these arguments', input)
         protocol.run_dkg_protocol(input.validators_info, input.user_sig_req_seed, input.x25519_private_key).then((output) => {
-          console.log('OUTPUT:', output)
+          console.log(output)
         }).catch((err) => {
-          console.log('ERR', err)
+          console.error('ERR', err)
         })
         break
     case 'sign':
         console.log('Starting signing protocol with these arguments', input)
         protocol.run_signing_protocol(input.key_share, input.sig_uid, input.validators_info, input.user_sig_req_seed, input.x25519_private_key).then((output) => {
-            console.log('OUTPUT:', output)
+            console.log(output)
         }).catch((err) => {
             console.error('ERR', err)
         })
