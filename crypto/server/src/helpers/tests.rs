5f72bad50a4037bad7f3cf9b6d4ded1e3ae4fdec
Hernando Castano <hernando@hcastano.com>
2023-11-15 12:54:50 -0500

    By default filter out logs that aren't `ERROR` level

--- crypto/server/src/helpers/tests.rs
+++ crypto/server/src/helpers/tests.rs
@@ -42,7 +42,11 @@ lazy_static::lazy_static! {
     /// Since this only needs to be initialized once for the whole test suite we define it as a lazy
     /// static.
     pub static ref LOGGER: () = {
-        tracing_subscriber::fmt().init();
+        // We set up the tests to only print out logs of `ERROR` or higher by default, otherwise we
+        // fall back to the user's `RUST_LOG` settings.
+        tracing_subscriber::fmt()
+            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+            .init();
     };
 }
 
