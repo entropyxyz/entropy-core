2d875dbe8c4104adc9cfe045852927133d73bb32
Hernando Castano <hernando@hcastano.com>
2023-11-21 15:16:06 -0500

    Remove event from health check
    Don't think we need this since we get some events around the `request` and `response` parts of this
    from the Tower HTTP middleware.

--- crypto/server/src/health/api.rs
+++ crypto/server/src/health/api.rs
@@ -3,6 +3,5 @@ use axum::http::StatusCode;
 /// For checking the health of the TSS
 #[tracing::instrument]
 pub async fn healthz() -> StatusCode {
-    tracing::info!("Sucesfully performed health check");
     StatusCode::OK
 }
