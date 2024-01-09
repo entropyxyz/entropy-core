b3618506003fd670a3004d1a8965323ef96c64b7
JesseAbram <33698952+JesseAbram@users.noreply.github.com>
2024-01-09 06:28:25 -0800

    Update crates/testing-utils/src/constants.rs
    Co-authored-by: peg <peg@magmacollective.org>

--- crates/testing-utils/src/constants.rs
+++ crates/testing-utils/src/constants.rs
@@ -47,7 +47,7 @@ lazy_static! {
 /// `example_barebones_with_auxilary.wasm` from the `constraints` repo.
 pub const TEST_PROGRAM_WASM_BYTECODE: &[u8] =
     include_bytes!("../example_barebones_with_auxilary.wasm");
-/// `infinite_loop.wasm` from the `constraints` repo.
+/// `infinite_loop.wasm` from the `programs` repo.
 pub const TEST_INFINITE_LOOP_BYTECODE: &[u8] = include_bytes!("../infinite_loop.wasm");
 /// `example_custom_hash.wasm` from the `constraints` repo.
 pub const TEST_PROGRAM_CUSTOM_HASH: &[u8] = include_bytes!("../example_custom_hash.wasm");
