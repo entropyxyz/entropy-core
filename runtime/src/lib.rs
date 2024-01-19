08e89ff6d47078f359d561bc93a6746efbdc8156
Hernando Castano <hernando@hcastano.com>
2024-01-19 16:25:27 -0700

    Add `opaque` types to runtime
    These match what was in `node-primitives`. In particular we're interested in the `Block` type which
    we were using in a lot of places, and I incorrectly changed to be `Unchechecked` instead of
    `Opaque`.

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -166,6 +166,30 @@ pub type Nonce = u32;
 /// A hash of some data used by the chain.
 pub type Hash = sp_core::H256;
 
+/// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
+/// the specifics of the runtime. They can then be made to be agnostic over specific formats
+/// of data like extrinsics, allowing for them to continue syncing the network through upgrades
+/// to even the core data structures.
+pub mod opaque {
+    use super::*;
+
+    pub use sp_runtime::OpaqueExtrinsic as UncheckedExtrinsic;
+
+    /// Opaque block header type.
+    pub type Header = generic::Header<BlockNumber, BlakeTwo256>;
+    /// Opaque block type.
+    pub type Block = generic::Block<Header, UncheckedExtrinsic>;
+    /// Opaque block identifier type.
+    pub type BlockId = generic::BlockId<Block>;
+
+    // impl_opaque_keys! {
+    //     pub struct SessionKeys {
+    //         pub aura: Aura,
+    //         pub grandpa: Grandpa,
+    //     }
+    // }
+}
+
 /// Runtime version.
 #[sp_version::runtime_version]
 pub const VERSION: RuntimeVersion = RuntimeVersion {
