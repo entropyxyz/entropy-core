b8cf290282b6132294866d1306e083f8e58ce49a
Hernando Castano <hernando@hcastano.com>
2024-01-23 13:42:18 -0700

    Bump `spec_version` and `transaction_version`

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -207,7 +207,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     // We update this if the runtime behaviour has changed. When this happens we set the
     // `impl_version` to `0`.
     #[allow(clippy::zero_prefixed_literal)]
-    spec_version: 009,
+    spec_version: 010,
 
     // We only bump this if the runtime behaviour remains unchanged, but the implementations details
     // have changed.
@@ -221,7 +221,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     // call index, parameter changes, etc.).
     //
     // The `spec_version` also needs to be bumped in this case.
-    transaction_version: 3,
+    transaction_version: 4,
 
     // Version of the state implementation to use.
     //
