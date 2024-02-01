5debc7292f4ccac454a9497ca2c1dcb289d60e6c
Hernando Castano <hernando@hcastano.com>
2024-01-31 17:53:13 -0700

    Clean up script to generate node keys

--- scripts/node-keys.sh
+++ scripts/node-keys.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
-set -eux
+#
+# A script used to generate all the keys needed for validators.
+#
+# **This should not be used anywhere sensitive, it prints out your new seed phrase!**
+
+set -eu
 
 secretPhrase=$(cargo run -p entropy -- key generate --output-type json | jq -r .secretPhrase)
 
-./target/debug/entropy key inspect "$secretPhrase//controller" --output-type json
-./target/debug/entropy key inspect "$secretPhrase//stash" --output-type json
-./target/debug/entropy key inspect "$secretPhrase//babe" --output-type json
-./target/debug/entropy key inspect "$secretPhrase//imon" --output-type json
-./target/debug/entropy key inspect "$secretPhrase//audi" --output-type json
+# Not safe, I know...but this is a development tool.
+echo -e "Secret Phrase: $secretPhrase\n"
 
-./target/debug/entropy key inspect "$secretPhrase//gran" --scheme ed25519 --output-type json
+declare -A keyTypes=(
+  ["controller"]="Sr25519"
+  ["stash"]="Sr25519"
+  ["babe"]="Sr25519"
+  ["imon"]="Sr25519"
+  ["audi"]="Sr25519"
+  ["gran"]="Ed25519"
+)
+
+for name in "${!keyTypes[@]}"; do
+  scheme="${keyTypes[$name]}"
+  ./target/debug/entropy key inspect "$secretPhrase//$name" --scheme $scheme
+  echo
+done
