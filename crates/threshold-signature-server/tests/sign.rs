02645a012d0a78aa3cca256f48b8c59d06283249
peg <ameba23@systemli.org>
2024-02-06 15:30:51 +0100

    Add integration test for public

--- crates/threshold-signature-server/tests/sign.rs
+++ crates/threshold-signature-server/tests/sign.rs
@@ -29,6 +29,7 @@ use entropy_testing_utils::{
 use serial_test::serial;
 use sp_core::crypto::Ss58Codec;
 use sp_keyring::AccountKeyring;
+use subxt::utils::AccountId32 as SubxtAccountId32;
 use synedrion::k256::ecdsa::VerifyingKey;
 
 use entropy_tss::{
@@ -40,7 +41,7 @@ use entropy_tss::{
 #[serial]
 async fn integration_test_sign() {
     clean_tests();
-    let pre_registered_user = AccountKeyring::Dave;
+    let pre_registered_user = AccountKeyring::Ferdie;
     let eve = AccountKeyring::Eve;
 
     let signing_address = pre_registered_user.to_account_id().to_ss58check();
@@ -134,6 +135,63 @@ async fn integration_test_sign_private() {
         None,
         PREIMAGE_SHOULD_SUCCEED.to_vec(),
         keyshare_option.clone(),
+        Some(AUXILARY_DATA_SHOULD_SUCCEED.to_vec()),
+    )
+    .await
+    .unwrap();
+
+    let recovery_key_from_sig = VerifyingKey::recover_from_prehash(
+        &message_should_succeed_hash,
+        &recoverable_signature.signature,
+        recoverable_signature.recovery_id,
+    )
+    .unwrap();
+    assert_eq!(keyshare_option.clone().unwrap().verifying_key(), recovery_key_from_sig);
+}
+
+#[tokio::test]
+#[serial]
+async fn integration_test_sign_public() {
+    clean_tests();
+    let pre_registered_public_user = AccountKeyring::Dave;
+    let request_author = AccountKeyring::One;
+    let eve = AccountKeyring::Eve;
+
+    let signing_address = pre_registered_public_user.to_account_id().to_ss58check();
+    let (_validator_ips, _validator_ids, keyshare_option) =
+        spawn_testing_validators(Some(signing_address.clone()), false).await;
+    let substrate_context = test_context_stationary().await;
+    let api = get_api(&substrate_context.node_proc.ws_url).await.unwrap();
+    let rpc = get_rpc(&substrate_context.node_proc.ws_url).await.unwrap();
+
+    let program_pointer = test_client::store_program(
+        &api,
+        &eve.pair(),
+        TEST_PROGRAM_WASM_BYTECODE.to_owned(),
+        vec![],
+    )
+    .await
+    .unwrap();
+
+    test_client::update_programs(
+        &api,
+        &rpc,
+        &pre_registered_public_user.pair(),
+        &pre_registered_public_user.pair(),
+        BoundedVec(vec![ProgramInstance { program_pointer, program_config: vec![] }]),
+    )
+    .await
+    .unwrap();
+
+    let message_should_succeed_hash = Hasher::keccak(PREIMAGE_SHOULD_SUCCEED);
+
+    let recoverable_signature = test_client::sign(
+        &api,
+        &rpc,
+        request_author.pair(),
+        Some(SubxtAccountId32(pre_registered_public_user.public().0)),
+        PREIMAGE_SHOULD_SUCCEED.to_vec(),
+        None,
         Some(AUXILARY_DATA_SHOULD_SUCCEED.to_vec()),
     )
     .await
