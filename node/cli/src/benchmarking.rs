381a1635c008ce75f7589d8ea1d4add608fdc5a2
Hernando Castano <hernando@hcastano.com>
2024-03-06 17:42:29 -0700

    Remove benchmarks

--- node/cli/src/benchmarking.rs
+++ node/cli/src/benchmarking.rs
@@ -147,7 +147,6 @@ pub fn create_benchmark_extrinsic(
         frame_system::CheckNonce::<runtime::Runtime>::from(nonce),
         frame_system::CheckWeight::<runtime::Runtime>::new(),
         pallet_transaction_payment::ChargeTransactionPayment::<runtime::Runtime>::from(0),
-        pallet_free_tx::ValidateElectricityPayment::<runtime::Runtime>::new(),
         pallet_relayer::ValidateConfirmRegistered::<runtime::Runtime>::new(),
     );
 
