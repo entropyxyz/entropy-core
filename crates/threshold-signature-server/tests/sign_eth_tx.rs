208a816984e3b823c67fe585701ce340649e29b3
peg <ameba23@systemli.org>
2024-03-08 09:57:40 +0100

    Add test for signing an ethereum transaction

---
+++ crates/threshold-signature-server/tests/sign_eth_tx.rs
@@ -0,0 +1,135 @@
+// Copyright (C) 2023 Entropy Cryptography Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use entropy_kvdb::clean_tests;
+use entropy_protocol::RecoverableSignature;
+use entropy_testing_utils::{
+    chain_api::{
+        entropy::runtime_types::bounded_collections::bounded_vec::BoundedVec,
+        entropy::runtime_types::pallet_registry::pallet::ProgramInstance,
+    },
+    constants::{AUXILARY_DATA_SHOULD_SUCCEED, TEST_PROGRAM_WASM_BYTECODE},
+    substrate_context::test_context_stationary,
+    test_client,
+    tss_server_process::spawn_testing_validators,
+};
+use entropy_tss::{
+    chain_api::{get_api, get_rpc},
+    common::Hasher,
+};
+use ethers::core::{
+    abi::ethabi::ethereum_types::H160,
+    types::{Transaction, TransactionRequest, U256},
+    utils::rlp::{Decodable, Rlp},
+};
+use serial_test::serial;
+use sp_core::crypto::Ss58Codec;
+use sp_keyring::AccountKeyring;
+use std::str::FromStr;
+use synedrion::k256::ecdsa::VerifyingKey;
+
+#[tokio::test]
+#[serial]
+async fn integration_test_sign_eth_tx() {
+    clean_tests();
+    let pre_registered_user = AccountKeyring::Ferdie;
+    let deployer = AccountKeyring::Eve;
+
+    let signing_address = pre_registered_user.to_account_id().to_ss58check();
+    let (_validator_ips, _validator_ids, keyshare_option) =
+        spawn_testing_validators(Some(signing_address.clone()), false).await;
+    let substrate_context = test_context_stationary().await;
+    let api = get_api(&substrate_context.node_proc.ws_url).await.unwrap();
+    let rpc = get_rpc(&substrate_context.node_proc.ws_url).await.unwrap();
+
+    let program_pointer = test_client::store_program(
+        &api,
+        &rpc,
+        &deployer.pair(),
+        TEST_PROGRAM_WASM_BYTECODE.to_owned(),
+        vec![],
+    )
+    .await
+    .unwrap();
+
+    test_client::update_programs(
+        &api,
+        &rpc,
+        &pre_registered_user.pair(),
+        &pre_registered_user.pair(),
+        BoundedVec(vec![ProgramInstance { program_pointer, program_config: vec![] }]),
+    )
+    .await
+    .unwrap();
+
+    // Get the public key to use in the 'from' field
+    let verifying_key = keyshare_option.clone().unwrap().verifying_key();
+
+    let transaction_request = create_unsigned_eth_tx(verifying_key);
+    let message = transaction_request.rlp_unsigned().to_vec();
+
+    let message_hash = Hasher::keccak(&message);
+
+    let recoverable_signature = test_client::sign(
+        &api,
+        &rpc,
+        pre_registered_user.pair(),
+        None,
+        message,
+        None,
+        Some(AUXILARY_DATA_SHOULD_SUCCEED.to_vec()),
+    )
+    .await
+    .unwrap();
+
+    let recovery_key_from_sig = VerifyingKey::recover_from_prehash(
+        &message_hash,
+        &recoverable_signature.signature,
+        recoverable_signature.recovery_id,
+    )
+    .unwrap();
+    assert_eq!(verifying_key, recovery_key_from_sig);
+
+    let ethers_signature = recoverable_signature_to_ethers_signature(recoverable_signature);
+    let signed_transaction_bytes = transaction_request.rlp_signed(&ethers_signature);
+    let rlp = Rlp::new(&signed_transaction_bytes);
+    let transaction = Transaction::decode(&rlp).unwrap();
+    let recovered_eth_address = transaction.recover_from().unwrap();
+    assert_eq!(recovered_eth_address, verifying_key_to_h160(verifying_key));
+}
+
+fn verifying_key_to_h160(verifying_key: VerifyingKey) -> H160 {
+    let encoded_point = verifying_key.to_encoded_point(true);
+    let hashed_public_key = Hasher::keccak(encoded_point.as_bytes());
+    let truncated_hash: [u8; 20] = hashed_public_key[12..].try_into().unwrap();
+    H160(truncated_hash)
+}
+
+fn recoverable_signature_to_ethers_signature(
+    recoverable_signature: RecoverableSignature,
+) -> ethers::core::types::Signature {
+    let recovery_id_u64: u64 = recoverable_signature.recovery_id.to_byte().into();
+    let v: u64 = 27 + recovery_id_u64;
+    let r = U256::from_big_endian(&recoverable_signature.signature.r().to_bytes());
+    let s = U256::from_big_endian(&recoverable_signature.signature.s().to_bytes());
+
+    ethers::core::types::Signature { r, s, v }
+}
+
+fn create_unsigned_eth_tx(verifying_key: VerifyingKey) -> TransactionRequest {
+    let from = verifying_key_to_h160(verifying_key);
+    let to = H160::from_str("772b9a9e8aa1c9db861c6611a82d251db4fac990").unwrap();
+    TransactionRequest::pay(to, 1000).from(from)
+}
