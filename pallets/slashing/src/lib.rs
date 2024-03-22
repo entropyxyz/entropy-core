86bc627432c9c3ec35ec9f16fe71559821fa5f9e
Hernando Castano <hernando@hcastano.com>
2024-03-22 16:14:13 -0600

    Add missing comment

--- pallets/slashing/src/lib.rs
+++ pallets/slashing/src/lib.rs
@@ -69,6 +69,7 @@ pub mod pallet {
     pub trait Config: frame_system::Config {
         type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 
+        /// The identifier type for an authority.
         type AuthorityId: Member
             + Parameter
             + RuntimeAppPublic
