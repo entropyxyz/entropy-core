933d546bef8b89913e11656996043f0a292030da
peg <ameba23@systemli.org>
2024-03-28 17:12:19 +0100

    Fix JS test

--- crates/protocol/nodejs-test/index.test.js
+++ crates/protocol/nodejs-test/index.test.js
@@ -11,13 +11,12 @@ test('Convert Uint8Array to and from hex', function (t) {
   t.true(protocol.constantTimeEq(empty, emptyArray))
 })
 
-test('Encrypt, decrypt with X25519Chacha20Poly1305', function (t) {
-  t.plan(7)
+test('Encrypt, decrypt with HPKE', function (t) {
+  t.plan(2)
 
   const { generateSigningKey, publicKeyFromSecret, encryptAndSign, decryptAndVerify } = protocol.Hpke
 
   const aliceSk = generateSigningKey()
-  const alicePk = publicKeyFromSecret(aliceSk)
 
   const bobSk = generateSigningKey()
   const bobPk = publicKeyFromSecret(bobSk)
@@ -27,17 +26,14 @@ test('Encrypt, decrypt with X25519Chacha20Poly1305', function (t) {
   // Alice encrypts and signs the message to bob.
   const encryptedAndSignedMessage = encryptAndSign(aliceSk, plaintext, bobPk)
 
-  const { sig, pk, a, nonce, msg, recip } = JSON.parse(encryptedAndSignedMessage)
-  t.equals(protocol.fromHex(sig).length, 64)
-  t.equals(nonce.length, 12)
-  t.equals(pk.length, 32)
-  t.true(protocol.constantTimeEq(a, alicePk))
-  t.true(protocol.constantTimeEq(recip, bobPk))
-  t.equals(protocol.fromHex(msg).length, plaintext.length + 16) // MAC is 16 bytes
-
   // Bob decrypts the message.
   const decryptedPlaintext = decryptAndVerify(bobSk, encryptedAndSignedMessage)
 
   // Check the original plaintext equals the decrypted plaintext.
   t.true(protocol.constantTimeEq(decryptedPlaintext, plaintext))
+
+  // const mallorySk = generateSigningKey()
+
+  // Malloy cannot decrypt the message.
+  // t.throws(decryptAndVerify(mallorySk, encryptedAndSignedMessage), /OpenError/)
 })
