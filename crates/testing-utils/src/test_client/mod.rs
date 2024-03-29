f2e7d84c31105dbedd640a48798979d12b8a3842
Jesse Abramowitz <jesse@entropy.xyz>
2024-03-29 10:44:52 -0500

    fmt

--- crates/testing-utils/src/test_client/mod.rs
+++ crates/testing-utils/src/test_client/mod.rs
@@ -263,8 +263,11 @@ pub async fn store_program(
     configuration_interface: Vec<u8>,
     auxiliary_data_interface: Vec<u8>,
 ) -> anyhow::Result<<EntropyConfig as Config>::Hash> {
-    let update_program_tx =
-        entropy::tx().programs().set_program(program, configuration_interface, auxiliary_data_interface);
+    let update_program_tx = entropy::tx().programs().set_program(
+        program,
+        configuration_interface,
+        auxiliary_data_interface,
+    );
     let deployer = PairSigner::<EntropyConfig, sr25519::Pair>::new(deployer_pair.clone());
 
     let in_block = submit_transaction(api, rpc, &deployer, &update_program_tx, None).await?;
