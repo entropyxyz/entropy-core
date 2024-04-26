95b6d282382d38013bb5620d5cc4e38c18931197
peg <ameba23@systemli.org>
2024-04-26 11:05:45 +0200

    Doccomment

--- crates/shared/src/user.rs
+++ crates/shared/src/user.rs
@@ -3,6 +3,9 @@ use serde::{Deserialize, Serialize};
 use subxt::utils::AccountId32;
 
 /// Details of a TSS server
+/// This is different from `entropy_shared::ValidatorInfo` in that it is used for interacting
+/// with the client rather than with the chain - since it uses types which we cannot use in the
+/// chain runtime
 #[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
 pub struct ValidatorInfo {
     pub x25519_public_key: X25519PublicKey,
