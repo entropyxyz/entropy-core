4cc1ecd7413b5737260b70f8111e2b943aa5638b
peg <ameba23@systemli.org>
2024-04-29 15:05:39 +0200

    Make it possible to submit extrinsics on wasm

--- crates/client/src/substrate.rs
+++ crates/client/src/substrate.rs
@@ -13,79 +13,74 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //! For interacting with the substrate chain node
-use crate::chain_api::EntropyConfig;
+use crate::chain_api::{entropy, EntropyConfig};
+use entropy_shared::MORTALITY_BLOCKS;
+use sp_core::{sr25519, Pair};
 use subxt::{
     backend::legacy::LegacyRpcMethods,
+    blocks::ExtrinsicEvents,
+    config::PolkadotExtrinsicParamsBuilder as Params,
     storage::address::{StorageAddress, Yes},
-    utils::H256,
-    OnlineClient,
+    tx::{Signer, TxPayload, TxStatus},
+    utils::{AccountId32, MultiSignature, H256},
+    Config, OnlineClient,
 };
 use thiserror::Error;
 
-#[cfg(feature = "native")]
-pub use submit::submit_transaction;
+/// Send a transaction to the Entropy chain
+///
+/// Optionally takes a nonce, otherwise it grabs the latest nonce from the chain
+///
+pub async fn submit_transaction<Call: TxPayload, S: Signer<EntropyConfig>>(
+    api: &OnlineClient<EntropyConfig>,
+    rpc: &LegacyRpcMethods<EntropyConfig>,
+    signer: &S,
+    call: &Call,
+    nonce_option: Option<u32>,
+) -> Result<ExtrinsicEvents<EntropyConfig>, SubstrateError> {
+    let block_hash = rpc.chain_get_block_hash(None).await?.ok_or(SubstrateError::BlockHash)?;
 
-/// Currently not available on wasm due to needing subxt::tx::PairSigner
-#[cfg(feature = "native")]
-mod submit {
-    use super::SubstrateError;
-    use crate::chain_api::{entropy, EntropyConfig};
-    use entropy_shared::MORTALITY_BLOCKS;
-    use sp_core::sr25519;
-    use subxt::{
-        backend::legacy::LegacyRpcMethods,
-        blocks::ExtrinsicEvents,
-        config::PolkadotExtrinsicParamsBuilder as Params,
-        tx::{PairSigner, TxPayload, TxStatus},
-        OnlineClient,
+    let nonce = if let Some(nonce) = nonce_option {
+        nonce
+    } else {
+        let nonce_call =
+            entropy::apis().account_nonce_api().account_nonce(signer.account_id().clone());
+        api.runtime_api().at(block_hash).call(nonce_call).await?
     };
 
-    /// Send a transaction to the Entropy chain
-    ///
-    /// Optionally takes a nonce, otherwise it grabs the latest nonce from the chain
-    ///
-    pub async fn submit_transaction<Call: TxPayload>(
-        api: &OnlineClient<EntropyConfig>,
-        rpc: &LegacyRpcMethods<EntropyConfig>,
-        signer: &PairSigner<EntropyConfig, sr25519::Pair>,
-        call: &Call,
-        nonce_option: Option<u32>,
-    ) -> Result<ExtrinsicEvents<EntropyConfig>, SubstrateError> {
-        let block_hash = rpc.chain_get_block_hash(None).await?.ok_or(SubstrateError::BlockHash)?;
+    let latest_block = api.blocks().at_latest().await?;
+    let tx_params =
+        Params::new().mortal(latest_block.header(), MORTALITY_BLOCKS).nonce(nonce.into()).build();
+    let mut tx = api.tx().create_signed(call, signer, tx_params).await?.submit_and_watch().await?;
 
-        let nonce = if let Some(nonce) = nonce_option {
-            nonce
-        } else {
-            let nonce_call =
-                entropy::apis().account_nonce_api().account_nonce(signer.account_id().clone());
-            api.runtime_api().at(block_hash).call(nonce_call).await?
+    while let Some(status) = tx.next().await {
+        match status? {
+            TxStatus::InBestBlock(tx_in_block) | TxStatus::InFinalizedBlock(tx_in_block) => {
+                return Ok(tx_in_block.wait_for_success().await?);
+            },
+            TxStatus::Error { message }
+            | TxStatus::Invalid { message }
+            | TxStatus::Dropped { message } => {
+                // Handle any errors:
+                return Err(SubstrateError::BadEvent(message));
+            },
+            // Continue otherwise:
+            _ => continue,
         };
-
-        let latest_block = api.blocks().at_latest().await?;
-        let tx_params = Params::new()
-            .mortal(latest_block.header(), MORTALITY_BLOCKS)
-            .nonce(nonce.into())
-            .build();
-        let mut tx =
-            api.tx().create_signed(call, signer, tx_params).await?.submit_and_watch().await?;
-
-        while let Some(status) = tx.next().await {
-            match status? {
-                TxStatus::InBestBlock(tx_in_block) | TxStatus::InFinalizedBlock(tx_in_block) => {
-                    return Ok(tx_in_block.wait_for_success().await?);
-                },
-                TxStatus::Error { message }
-                | TxStatus::Invalid { message }
-                | TxStatus::Dropped { message } => {
-                    // Handle any errors:
-                    return Err(SubstrateError::BadEvent(message));
-                },
-                // Continue otherwise:
-                _ => continue,
-            };
-        }
-        Err(SubstrateError::NoEvent)
     }
+    Err(SubstrateError::NoEvent)
+}
+
+/// Send a transaction to the Entropy chain giving a sr25519::Pair to sign with
+pub async fn submit_transaction_with_pair<Call: TxPayload>(
+    api: &OnlineClient<EntropyConfig>,
+    rpc: &LegacyRpcMethods<EntropyConfig>,
+    pair: &sr25519::Pair,
+    call: &Call,
+    nonce_option: Option<u32>,
+) -> Result<ExtrinsicEvents<EntropyConfig>, SubstrateError> {
+    let signer = Sr25519Signer::new(pair.clone());
+    submit_transaction(api, rpc, &signer, call, nonce_option).await
 }
 
 /// Gets data from the Entropy chain
@@ -109,6 +104,35 @@ where
     let result = api.storage().at(block_hash).fetch(&storage_call).await?;
 
     Ok(result)
+}
+
+/// A wrapper around [sr25519::Pair] which implements [Signer]
+/// This is needed because on wasm we cannot use the generic `subxt::tx::PairSigner`
+#[derive(Clone)]
+struct Sr25519Signer {
+    account_id: <EntropyConfig as Config>::AccountId,
+    pair: sr25519::Pair,
+}
+
+impl Sr25519Signer {
+    /// Creates a new [`Sr25519Signer`] from an [`sr25519::Pair`].
+    pub fn new(pair: sr25519::Pair) -> Self {
+        Self { account_id: AccountId32(pair.public().0), pair }
+    }
+}
+
+impl Signer<EntropyConfig> for Sr25519Signer {
+    fn account_id(&self) -> <EntropyConfig as Config>::AccountId {
+        self.account_id.clone()
+    }
+
+    fn address(&self) -> <EntropyConfig as Config>::Address {
+        self.account_id.clone().into()
+    }
+
+    fn sign(&self, signer_payload: &[u8]) -> <EntropyConfig as Config>::Signature {
+        MultiSignature::Sr25519(self.pair.sign(signer_payload).0)
+    }
 }
 
 /// Error relating to submitting an extrinsic or querying the chain
