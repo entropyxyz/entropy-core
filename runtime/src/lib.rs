f7be89cdad00cc6142ad3bbb3f8505865eb0427b
peg <peg@magmacollective.org>
2024-05-02 10:00:46 +0200

    v0.0.12 Release  (#797)
    * Add description to entropy-shared Cargo.toml
    
    * Prepare protocol Cargo.toml
    
    * Downgrade zeroize
    
    * Update kvdb Cargo.toml
    
    * Update testing-utils Cargo.toml
    
    * Updates to Cargo.tomls
    
    * Bump version to v0.0.12
    
    * Update changelog
    
    * Bump spec version / transaction version
    
    * Run benchmarks
    
    * benchmarks
    
    * benchmarks
    
    * remove unused benches
    
    ---------
    
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-39-223.us-east-2.compute.internal>
    Co-authored-by: Jesse Abramowitz <jesse@entropy.xyz>

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -208,7 +208,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     // We update this if the runtime behaviour has changed. When this happens we set the
     // `impl_version` to `0`.
     #[allow(clippy::zero_prefixed_literal)]
-    spec_version: 011,
+    spec_version: 012,
 
     // We only bump this if the runtime behaviour remains unchanged, but the implementations details
     // have changed.
@@ -222,7 +222,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     // call index, parameter changes, etc.).
     //
     // The `spec_version` also needs to be bumped in this case.
-    transaction_version: 5,
+    transaction_version: 6,
 
     // Version of the state implementation to use.
     //
