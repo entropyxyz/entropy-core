7bc3947db7b94df4cdc5586580063eb13c20e993
JesseAbram <33698952+JesseAbram@users.noreply.github.com>
2024-05-09 07:22:32 -0700

    Add rpc insecure url (#826)
    * add rpc insecure url
    
    * test
    
    * test
    
    * remove test

--- crates/client/src/chain_api.rs
+++ crates/client/src/chain_api.rs
@@ -45,7 +45,8 @@ pub async fn get_api(url: &str) -> Result<OnlineClient<EntropyConfig>, subxt::Er
 /// Creates a rpc instance to talk to chain
 /// Chain endpoint set on launch
 pub async fn get_rpc(url: &str) -> Result<LegacyRpcMethods<EntropyConfig>, subxt::Error> {
-    let rpc_client = RpcClient::from_url(url).await?;
+    // insecure url is fine since binaries are on the same machine
+    let rpc_client = RpcClient::from_insecure_url(url).await?;
     let rpc_methods = LegacyRpcMethods::<EntropyConfig>::new(rpc_client);
     Ok(rpc_methods)
 }
