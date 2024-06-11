b90aa9db97f0f6a1480f622ca117e4ec38fd59d3
peg <ameba23@systemli.org>
2024-06-11 11:49:21 +0200

    Rm moved fn from test helpers

--- crates/threshold-signature-server/src/helpers/tests.rs
+++ crates/threshold-signature-server/src/helpers/tests.rs
@@ -43,7 +43,6 @@ use axum::{routing::IntoMakeService, Router};
 use entropy_kvdb::{encrypted_sled::PasswordMethod, get_db_path, kv_manager::KvManager};
 use entropy_protocol::{KeyParams, KeyShareWithAuxInfo, PartyId};
 use entropy_shared::DETERMINISTIC_KEY_SHARE_EVE;
-use entropy_testing_utils::tss_server_process::put_keyshare_in_db;
 use rand_core::OsRng;
 use sp_core::Pair;
 use subxt::{
@@ -206,63 +205,6 @@ pub async fn spawn_testing_validators(
 
     let ips = ports.iter().map(|port| format!("127.0.0.1:{port}")).collect();
     (ips, ids, user_keyshare_option)
-}
-
-/// Spawn 3 TSS nodes with no pre-exisiting keyshares
-pub async fn spawn_3_testing_validators() -> (Vec<String>, Vec<PartyId>) {
-    // spawn threshold servers
-    let ports = [3001i64, 3002, 3003];
-
-    let (alice_axum, alice_kv) =
-        create_clients("validator1".to_string(), vec![], vec![], &Some(ValidatorName::Alice)).await;
-    let alice_id = PartyId::new(SubxtAccountId32(
-        *get_signer(&alice_kv).await.unwrap().account_id().clone().as_ref(),
-    ));
-
-    let (bob_axum, bob_kv) =
-        create_clients("validator2".to_string(), vec![], vec![], &Some(ValidatorName::Bob)).await;
-    let bob_id = PartyId::new(SubxtAccountId32(
-        *get_signer(&bob_kv).await.unwrap().account_id().clone().as_ref(),
-    ));
-
-    let (charlie_axum, charlie_kv) =
-        create_clients("validator3".to_string(), vec![], vec![], &Some(ValidatorName::Charlie))
-            .await;
-    let charlie_id = PartyId::new(SubxtAccountId32(
-        *get_signer(&charlie_kv).await.unwrap().account_id().clone().as_ref(),
-    ));
-
-    let ids = vec![alice_id, bob_id, charlie_id];
-
-    put_keyshare_in_db("alice", alice_kv).await;
-    put_keyshare_in_db("bob", bob_kv).await;
-    put_keyshare_in_db("charlie", charlie_kv).await;
-
-    let listener_alice = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", ports[0]))
-        .await
-        .expect("Unable to bind to given server address.");
-    tokio::spawn(async move {
-        axum::serve(listener_alice, alice_axum).await.unwrap();
-    });
-
-    let listener_bob = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", ports[1]))
-        .await
-        .expect("Unable to bind to given server address.");
-    tokio::spawn(async move {
-        axum::serve(listener_bob, bob_axum).await.unwrap();
-    });
-
-    let listener_charlie = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", ports[2]))
-        .await
-        .expect("Unable to bind to given server address.");
-    tokio::spawn(async move {
-        axum::serve(listener_charlie, charlie_axum).await.unwrap();
-    });
-
-    tokio::time::sleep(Duration::from_secs(1)).await;
-
-    let ips = ports.iter().map(|port| format!("127.0.0.1:{port}")).collect();
-    (ips, ids)
 }
 
 /// Removes the program at the program hash
