8bb8507af4780d07833b04114ba1f934be2193df
JesseAbram <33698952+JesseAbram@users.noreply.github.com>
2024-06-26 09:36:37 -0700

    Fix failing kvdb tests (#911)
    * fix failing kvdb tests
    
    * fmt
    
    * Update crates/kvdb/src/lib.rs
    
    Co-authored-by: Hernando Castano <HCastano@users.noreply.github.com>
    
    * fix
    
    ---------
    
    Co-authored-by: Hernando Castano <HCastano@users.noreply.github.com>

--- crates/threshold-signature-server/src/node_info/tests.rs
+++ crates/threshold-signature-server/src/node_info/tests.rs
@@ -27,6 +27,7 @@ use serial_test::serial;
 #[tokio::test]
 #[serial]
 async fn version_test() {
+    clean_tests();
     initialize_test_logger().await;
     setup_client().await;
     let client = reqwest::Client::new();
@@ -35,11 +36,13 @@ async fn version_test() {
         response.text().await.unwrap(),
         format!("{}-{}", env!("CARGO_PKG_VERSION"), env!("VERGEN_GIT_DESCRIBE"))
     );
+    clean_tests();
 }
 
 #[tokio::test]
 #[serial]
 async fn hashes_test() {
+    clean_tests();
     initialize_test_logger().await;
     setup_client().await;
     let response = reqwest::get("http://127.0.0.1:3001/hashes").await.unwrap();
@@ -56,6 +59,7 @@ async fn hashes_test() {
             HashingAlgorithm::Custom(0),
         ]
     );
+    clean_tests();
 }
 
 #[tokio::test]
