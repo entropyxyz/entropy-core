1153989788f831760b78512b6e2d4a12f9bacdd4
Hernando Castano <hernando@hcastano.com>
2024-07-15 17:57:09 -0400

    Remove `KeyVisiblity` from registry tests

--- pallets/registry/src/tests.rs
+++ pallets/registry/src/tests.rs
@@ -14,7 +14,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 use codec::Encode;
-use entropy_shared::{KeyVisibility, NETWORK_PARENT_KEY, VERIFICATION_KEY_LENGTH};
+use entropy_shared::{NETWORK_PARENT_KEY, VERIFICATION_KEY_LENGTH};
 use frame_support::{
     assert_noop, assert_ok,
     dispatch::{GetDispatchInfo, Pays},
@@ -24,7 +24,6 @@ use frame_support::{
 use pallet_programs::ProgramInfo;
 use pallet_registry::Call as RegistryCall;
 use pallet_staking_extension::ServerInfo;
-use sp_core::H256;
 use sp_runtime::{
     traits::{Hash, SignedExtension},
     transaction_validity::{TransactionValidity, ValidTransaction},
