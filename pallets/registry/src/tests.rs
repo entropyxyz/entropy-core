851e3297659d1487e773f5cfea023c37e0048c6b
peg <ameba23@systemli.org>
2024-09-18 09:25:02 +0200

    Add comment following review

--- pallets/registry/src/tests.rs
+++ pallets/registry/src/tests.rs
@@ -300,6 +300,7 @@ fn it_jumps_the_network() {
         assert_ok!(Registry::jump_start_network(RuntimeOrigin::signed(1)));
         assert_eq!(
             Registry::jumpstart_dkg(0),
+            // From the mock genesis config
             vec![
                 ValidatorInfo {
                     x25519_public_key: [0; 32],
