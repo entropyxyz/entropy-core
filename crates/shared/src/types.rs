b39c69ccf1cfb30bda8f9632e8abbc6479bc1612
JesseAbram <33698952+JesseAbram@users.noreply.github.com>
2024-10-08 14:25:31 -0700

    Update crates/shared/src/types.rs
    Co-authored-by: Hernando Castano <HCastano@users.noreply.github.com>

--- crates/shared/src/types.rs
+++ crates/shared/src/types.rs
@@ -88,6 +88,7 @@ pub struct OcwMessageProactiveRefresh {
 pub struct OcwMessageAttestationRequest {
     /// The account ids of all TSS servers who must submit an attestation this block
     pub tss_account_ids: Vec<[u8; 32]>,
+    /// The block height at which this attestation request was made.
     pub block_number: BlockNumber,
 }
 
