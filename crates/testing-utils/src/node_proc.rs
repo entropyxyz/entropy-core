f8a87fd333dd514de2358adce150e1d496f1c5de
Jesse Abramowitz <jesse@entropy.xyz>
2024-10-31 13:16:40 -0400

    add unsafe force node key generation

--- crates/testing-utils/src/node_proc.rs
+++ crates/testing-utils/src/node_proc.rs
@@ -129,6 +129,7 @@ impl TestNodeProcessBuilder {
     {
         let mut cmd = process::Command::new(&self.node_path);
         cmd.env("RUST_LOG", "error").arg(&self.chain_type).arg("--tmp");
+        cmd.arg("--unsafe-force-node-key-generation");
         if self.force_authoring {
             cmd.arg("--force-authoring");
         }
