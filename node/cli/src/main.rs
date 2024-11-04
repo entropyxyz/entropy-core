cfdee8a1981302f46d5e401a0a6205e5c1d0a367
Jesse Abramowitz <jesse@entropy.xyz>
2024-11-04 13:00:47 -0500

    fmt

--- node/cli/src/main.rs
+++ node/cli/src/main.rs
@@ -33,13 +33,13 @@
 #![doc(html_logo_url = "https://entropy.xyz/assets/logo_02.png")]
 #![warn(missing_docs)]
 
-mod chain_spec;
-mod service;
 mod benchmarking;
+mod chain_spec;
 mod cli;
 mod command;
 mod endowed_accounts;
 mod rpc;
+mod service;
 
 fn main() -> sc_cli::Result<()> {
     command::run()
