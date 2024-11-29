22d573f088adba9fb95ae7e532f51413648ff309
peg <ameba23@systemli.org>
2024-11-29 09:28:36 +0100

    Add main fn to testing-utils which starts 4 chain nodes

---
+++ crates/testing-utils/src/main.rs
@@ -0,0 +1,11 @@
+use entropy_testing_utils::test_node_process_testing_state;
+use std::io;
+
+#[tokio::main]
+async fn main() {
+    let _ctx = test_node_process_testing_state(true).await;
+
+    let mut buffer = String::new();
+    let stdin = io::stdin(); // We get `Stdin` here.
+    stdin.read_line(&mut buffer).unwrap();
+}
