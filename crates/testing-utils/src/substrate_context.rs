e88cadebffd8bae15202603cdc26ad2b67767e69
peg <ameba23@systemli.org>
2024-11-29 09:59:02 +0100

    Use actual TSS endpoints

--- crates/testing-utils/src/substrate_context.rs
+++ crates/testing-utils/src/substrate_context.rs
@@ -117,7 +117,6 @@ pub async fn test_node_process_testing_state(
             .to_string(),
     );
     // reduses message from chain to same TSS cleaning up a lot of logging
-    let fuck_off_tss_ip = Some("127.0.0.1:4010".to_string());
     let result = test_node(
         AccountKeyring::Alice,
         "--chain=integration-tests".to_string(),
@@ -130,7 +129,7 @@ pub async fn test_node_process_testing_state(
         "--chain=integration-tests".to_string(),
         force_authoring,
         alice_bootnode.clone(),
-        fuck_off_tss_ip.clone(),
+        Some("127.0.0.1:3002".to_string()),
     )
     .await;
     let result_charlie = test_node_process_with(
@@ -138,7 +137,7 @@ pub async fn test_node_process_testing_state(
         "--chain=integration-tests".to_string(),
         force_authoring,
         alice_bootnode.clone(),
-        fuck_off_tss_ip.clone(),
+        Some("127.0.0.1:3003".to_string()),
     )
     .await;
     let result_dave = test_node_process_with(
@@ -146,7 +145,7 @@ pub async fn test_node_process_testing_state(
         "--chain=integration-tests".to_string(),
         force_authoring,
         alice_bootnode.clone(),
-        fuck_off_tss_ip.clone(),
+        Some("127.0.0.1:3004".to_string()),
     )
     .await;
 
