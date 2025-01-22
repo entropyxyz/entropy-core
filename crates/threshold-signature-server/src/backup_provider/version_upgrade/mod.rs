4d59afd45b71276e151e7e7eb5ce40f10a26cf83
peg <ameba23@systemli.org>
2025-01-22 12:59:38 +0100

    Comments

--- crates/threshold-signature-server/src/backup_provider/version_upgrade/mod.rs
+++ crates/threshold-signature-server/src/backup_provider/version_upgrade/mod.rs
@@ -20,6 +20,9 @@ use axum::{extract::State, Json};
 pub async fn backup_encrypted_db_for_version_upgrade(
     State(app_state): State<AppState>,
 ) -> Result<Json<()>, bool> {
+    // TODO this should take a nonce signed with the associated stash account to prove that the
+    // caller is the node operator
+
     let storage_path = app_state.kv_store.storage_path().to_path_buf();
     let key_provider_details = get_key_provider_details(storage_path).unwrap();
     let db_dump = app_state.kv_store.kv().export_db().await.unwrap();
