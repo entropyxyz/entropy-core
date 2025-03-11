23f64f270c91c0006b4d8ae867884376fc4a565d
Jesse Abramowitz <jesse@entropy.xyz>
2025-03-11 12:20:35 -0400

    clean

--- crates/threshold-signature-server/src/user/api.rs
+++ crates/threshold-signature-server/src/user/api.rs
@@ -459,6 +459,7 @@ pub async fn generate_network_key(
     }
 
     validate_jump_start(&data, &api, &rpc, &app_state.cache).await?;
+
     let app_state = app_state.clone();
     setup_dkg(api, &rpc, data, app_state).await?;
 
