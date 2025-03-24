0659791dce7b94a06cb5fec8cb0883612746087c
peg <ameba23@systemli.org>
2025-03-24 13:01:33 +0100

    Build devnet chainspec script takes a chainspec given as an argument

--- scripts/build-devnet-chainspec.sh
+++ scripts/build-devnet-chainspec.sh
@@ -1,2 +1,3 @@
 # This script is meant to be run once before launching the devnet to build the inital chain spec
-./target/release/entropy build-spec --disable-default-bootnode --raw --chain=devnet > chains/devnet.json
+CHAINSPEC=${1:-"devnet"}
+ENTROPY_TESTNET_TSS_IP=0.0.0.0:3001 ./target/release/entropy build-spec --disable-default-bootnode --raw --chain=$CHAINSPEC > chains/${CHAINSPEC}.json
