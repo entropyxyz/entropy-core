706a38877e16a9d5e44873153cb5c9ae0446d61f
peg <ameba23@systemli.org>
2025-05-23 16:35:52 +0200

    Update container build workflow

--- node/cli/src/command.rs
+++ node/cli/src/command.rs
@@ -74,6 +74,7 @@ impl SubstrateCli for Cli {
     // | devnet-local                       | Four nodes, four threshold servers, Alice Bob Charlie and Dave, Development Configuration, Docker Compatible |
     // | integration-tests                  | Two nodes, Four threshold servers, Alice and Bob, Development Configuration |
     // | testnet-local                      | Two Nodes, Two threshold servers, Alice and Bob, Testnet Configuration, Docker Compatible |
+    // | testnet-blank                      | Testnet chainspec configuration with 'blank' inputs which need to be filled in later |
     // | <my-testnet>-chainspec-inputs.json | Custom Testnet Configuration with given inputs from a specified JSON file |
     fn load_spec(&self, id: &str) -> Result<Box<dyn sc_service::ChainSpec>, String> {
         Ok(match id {
