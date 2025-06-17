cdf98b68e79147ef984ca38aebf48ae1d098d11a
JesseAbram <33698952+JesseAbram@users.noreply.github.com>
2025-06-17 13:09:52 -0400

    Fix ```Validate_jumpstart_fail``` (#1477)
    * test
    
    * fix
    
    * fix
    
    * workflow
    
    * clean

--- crates/threshold-signature-server/src/user/tests.rs
+++ crates/threshold-signature-server/src/user/tests.rs
@@ -2226,11 +2226,16 @@ async fn test_validate_jump_start_fail_repeated() {
         query_chain(&api, &rpc, jump_start_progress_query, None).await.unwrap().unwrap();
     let validators_info: Vec<_> = jump_start_progress.into_iter().map(|v| v.0).collect();
 
-    let mut ocw_message = OcwMessageDkg { validators_info, block_number };
+    let mut ocw_message = OcwMessageDkg { validators_info, block_number: query_block };
     let err_stale_data = validate_jump_start(&ocw_message, &api, &rpc, &app_state.cache)
         .await
         .map_err(|e| e.to_string());
-    assert_eq!(err_stale_data, Err("Data is repeated".to_string()));
+
+    if block_number == query_block {
+        assert_eq!(err_stale_data, Err("Data is repeated".to_string()));
+    } else {
+        assert_eq!(err_stale_data, Err("Data is stale".to_string()));
+    }
 
     ocw_message.block_number = 1;
 
